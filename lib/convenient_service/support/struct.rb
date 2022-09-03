6eccf8318193773a1fefe0d436fe7e9cbd7270fa
Marian13 <mariankostyk13895@gmail.com>
2022-09-03 15:37:06 +0300

    fix(struct): use class new for backport

--- lib/convenient_service/support/struct.rb
+++ lib/convenient_service/support/struct.rb
@@ -2,34 +2,37 @@
 
 module ConvenientService
   module Support
+    ##
+    # Allows to `keyword_init' for Rubies lower than 2.5.
+    #
     class Struct < ::Struct
       if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.0.0") && Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.5.0")
         ##
         # NOTE: Copied with cosmetic modifications from:
         # https://github.com/marcandre/backports/blob/d422e696791317b3e8cf5af436b46a33ae276f70/lib/backports/2.5.0/struct/new.rb
         #
-        # TODO: Test on Ruby lower than "2.5.0"
-        #
-        def new(*members, keyword_init: false, &block)
-          klass = super(*members)
+        class << self
+          def new(*members, keyword_init: false, &block)
+            klass = super(*members)
 
-          if keyword_init
-            members.shift unless members.first.is_a?(Symbol)
+            if keyword_init
+              members.shift unless members.first.is_a?(Symbol)
 
-            arg_list = members.map { |m| "#{m}: nil" }.join(", ")
+              arg_list = members.map { |m| "#{m}: nil" }.join(", ")
 
-            setter = members.map { |m| "self.#{m} = #{m} " }.join("\n")
+              setter = members.map { |m| "self.#{m} = #{m} " }.join("\n")
 
-            <<~RUBY.tap { |code| klass.class_eval(code, __FILE__, __LINE__ + 1) }
-              def initialize(#{arg_list})
-                #{setter}
-              end
-            RUBY
+              <<~RUBY.tap { |code| klass.class_eval(code, __FILE__, __LINE__ + 1) }
+                def initialize(#{arg_list})
+                  #{setter}
+                end
+              RUBY
+            end
+
+            klass.class_eval(&block) if block
+
+            klass
           end
-
-          klass.class_eval(&block) if block
-
-          klass
         end
       end
     end
