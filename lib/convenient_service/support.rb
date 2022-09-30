17468dad480d03869c0c73618fc37502f1d74cfd
Marian13 <mariankostyk13895@gmail.com>
2022-10-01 00:34:10 +0300

    refactor(support): remove unused support

--- lib/convenient_service/support.rb
+++ lib/convenient_service/support.rb
@@ -2,13 +2,11 @@
 
 require_relative "support/concern"
 
-require_relative "support/pattern_matching"
 require_relative "support/abstract_method"
 require_relative "support/castable"
 require_relative "support/command"
 require_relative "support/copyable"
 require_relative "support/delegate"
 require_relative "support/finite_loop"
-require_relative "support/infinitely_nested_hash"
 require_relative "support/middleware"
 require_relative "support/struct"
