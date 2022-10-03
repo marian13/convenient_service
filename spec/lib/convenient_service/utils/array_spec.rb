a8d9bdb26487e51799dbb6c229f54785b4bfec58
Marian13 <mariankostyk13895@gmail.com>
2022-10-04 00:42:14 +0300

    test(utils): add specs for Utils::Array.drop_while

--- spec/lib/convenient_service/utils/array_spec.rb
+++ spec/lib/convenient_service/utils/array_spec.rb
@@ -115,6 +115,53 @@ RSpec.describe ConvenientService::Utils::Array do
     end
   end
 
+  describe ".drop_while" do
+    include ConvenientService::RSpec::Matchers::DelegateTo
+
+    let(:array) { [1, 2, 3, 4, 5] }
+    let(:condition_block) { proc { |item| item != 3 } }
+
+    context "when `inclusively` is NOT passed" do
+      subject(:result) { described_class.drop_while(array, &condition_block) }
+
+      it "defaults to `false`" do
+        ##
+        # NOTE: Same result as in "when `inclusively` is `false`".
+        # TODO: Shared example?
+        #
+        expect(result).to eq([3, 4, 5])
+      end
+    end
+
+    context "when `inclusively` is passed" do
+      subject(:result) { described_class.drop_while(array, inclusively: inclusively, &condition_block) }
+
+      context "when `inclusively` is `false`" do
+        let(:inclusively) { false }
+
+        specify {
+          expect { result }.to delegate_to(array, :drop_while).with_arguments(&condition_block)
+        }
+
+        it "returns array without items until condition is met for the first time" do
+          expect(result).to eq([3, 4, 5])
+        end
+      end
+
+      context "when `inclusively` is `true`" do
+        let(:inclusively) { true }
+
+        specify {
+          expect { result }.to delegate_to(array, :drop_while).with_arguments(&condition_block)
+        }
+
+        it "returns array without items until condition is met for the first time" do
+          expect(result).to eq([4, 5])
+        end
+      end
+    end
+  end
+
   describe ".find_last" do
     subject(:result) { described_class.find_last(array) { |item| item[0] == "b" } }
 
