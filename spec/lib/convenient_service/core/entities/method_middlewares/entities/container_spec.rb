494f0cdcefe194b72f22e4764c3f369f3664a113
Marian13 <mariankostyk13895@gmail.com>
2022-10-27 21:02:59 +0300

    test(core): add Container specs

--- spec/lib/convenient_service/core/entities/method_middlewares/entities/container_spec.rb
+++ spec/lib/convenient_service/core/entities/method_middlewares/entities/container_spec.rb
@@ -4,81 +4,12 @@ require "spec_helper"
 
 require "convenient_service"
 
-# rubocop:disable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
 RSpec.describe ConvenientService::Core::Entities::MethodMiddlewares::Entities::Container do
-  include ConvenientService::RSpec::Matchers::DelegateTo
+  example_group "modules" do
+    include ConvenientService::RSpec::Matchers::IncludeModule
 
-  let(:container) { described_class.new(klass: klass) }
+    subject { described_class }
 
-  let(:klass) { service_class }
-  let(:service_class) { Class.new }
-
-  example_group "attributes" do
-    include ConvenientService::RSpec::Matchers::HaveAttrReader
-
-    subject { container }
-
-    it { is_expected.to have_attr_reader(:klass) }
-  end
-
-  example_group "instance_methods" do
-    let(:scope) { :instance }
-    let(:method) { :result }
-
-    describe "define_method_middlewares_caller!" do
-      before do
-        ##
-        # NOTE: Returns `true` when called for the first time, `false` for all the subsequent calls.
-        # NOTE: Used for `and_return_its_value`.
-        # https://github.com/marian13/convenient_service/blob/c5b3adc4a0edc2d631dd1f44f914c28eeafefe1d/lib/convenient_service/rspec/matchers/custom/delegate_to.rb#L105
-        #
-        container.define_method_middlewares_caller!(scope, method)
-      end
-
-      specify do
-        expect { container.define_method_middlewares_caller!(scope, method) }
-          .to delegate_to(ConvenientService::Core::Entities::MethodMiddlewares::Entities::Container::Commands::DefineMethodMiddlewaresCaller, :call)
-          .with_arguments(scope: scope, method: method, container: container)
-          .and_return_its_value
-      end
-    end
-
-    describe "#methods_middlewares_callers" do
-      specify do
-        expect { container.methods_middlewares_callers }
-          .to delegate_to(ConvenientService::Core::Entities::MethodMiddlewares::Entities::Container::Commands::ResolveMethodsMiddlewaresCallers, :call)
-          .with_arguments(container: container)
-          .and_return_its_value
-      end
-    end
-
-    example_group "comparison" do
-      describe "#==" do
-        context "when `other` has different class" do
-          let(:other) { 42 }
-
-          it "returns `false`" do
-            expect(container == other).to be_nil
-          end
-        end
-
-        context "when `other` has different `klass`" do
-          let(:other) { described_class.new(klass: Class.new) }
-
-          it "returns `false`" do
-            expect(container == other).to eq(false)
-          end
-        end
-
-        context "when `other` has same attributes" do
-          let(:other) { described_class.new(klass: klass) }
-
-          it "returns `true`" do
-            expect(container == other).to eq(true)
-          end
-        end
-      end
-    end
+    it { is_expected.to include_module(ConvenientService::Core::Entities::MethodMiddlewares::Entities::Container::Concern) }
   end
 end
-# rubocop:enable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
