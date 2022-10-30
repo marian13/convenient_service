930d5cee0e7ebb69e28a2130dec95310b3786c6d
Marian13 <mariankostyk13895@gmail.com>
2022-10-30 23:32:56 +0200

    test(core): add Caller constants specs

--- spec/lib/convenient_service/core/entities/config/entities/method_middlewares/entities/caller_spec.rb
+++ spec/lib/convenient_service/core/entities/config/entities/method_middlewares/entities/caller_spec.rb
@@ -5,6 +5,20 @@ require "spec_helper"
 require "convenient_service"
 
 RSpec.describe ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::Caller do
+  example_group "constants" do
+    describe "::INSTANCE_PREFIX" do
+      it "is equal to `\"self.class.\"`" do
+        expect(described_class::INSTANCE_PREFIX).to eq("self.class.")
+      end
+    end
+
+    describe "::CLASS_PREFIX" do
+      it "is equal to `\"self.class.\"`" do
+        expect(described_class::CLASS_PREFIX).to eq("")
+      end
+    end
+  end
+
   example_group "modules" do
     include ConvenientService::RSpec::Matchers::IncludeModule
 
