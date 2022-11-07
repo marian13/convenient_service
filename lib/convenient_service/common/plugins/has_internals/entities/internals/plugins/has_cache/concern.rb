34ca7b2c9fb052bca7539e1795d5ee8df391ec24
Marian13 <mariankostyk13895@gmail.com>
2022-11-08 01:22:53 +0200

    test(has_cache): add specs for internals HasCache instance methods

--- lib/convenient_service/common/plugins/has_internals/entities/internals/plugins/has_cache/concern.rb
+++ lib/convenient_service/common/plugins/has_internals/entities/internals/plugins/has_cache/concern.rb
@@ -7,14 +7,18 @@ module ConvenientService
         module Entities
           class Internals
             module Plugins
+              ##
+              # @internal
+              #   TODO: Consider to refactor into common plugin?
+              #
               module HasCache
-                ##
-                # TODO: Specs.
-                #
                 module Concern
                   include Support::Concern
 
                   instance_methods do
+                    ##
+                    # @return [ConvenientService::Support::Cache]
+                    #
                     def cache
                       @cache ||= Support::Cache.new
                     end
