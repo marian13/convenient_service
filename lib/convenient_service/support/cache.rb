b0c6b5d57b89c6886b27b05eaec81950cbfced7b
Marian13 <mariankostyk13895@gmail.com>
2022-11-07 23:35:49 +0200

    refactore(cache): move cache to support

--- lib/convenient_service/support/cache.rb
+++ lib/convenient_service/support/cache.rb
@@ -1,67 +1,124 @@
 # frozen_string_literal: true
 
+require_relative "cache/key"
+
 module ConvenientService
   module Support
-    ##
-    # TODO: Specs.
-    #
     class Cache
-      attr_reader :hash
+      ##
+      # @return [void]
+      #
+      def initialize(hash = {})
+        @hash = hash
+      end
 
-      def initialize
-        @hash = {}
+      class << self
+        ##
+        # @return [ConvenientService::Support::Cache::Key]
+        #
+        def key(...)
+          Cache::Key.new(...)
+        end
       end
 
       ##
-      # https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-exist-3F
+      # @return [Boolean]
+      #
+      # @internal
+      #   https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-exist-3F
       #
       def exist?(key)
         hash.has_key?(key)
       end
 
       ##
-      # https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-read
+      # @return [Object] Can be any type.
+      #
+      # @internal
+      #   https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-read
       #
       def read(key)
         hash[key]
       end
 
       ##
-      # NOTE: `alias_method` is NOT used in order to have an ability to use `allow(cache).to receive(:read).with(key).and_call_original` for both `cache[key]` and `cache.read(key)` in RSpec.
+      # @return [Object] Can be any type.
+      #
+      # @internal
+      #   NOTE: `alias_method` is NOT used in order to have an ability to use `allow(cache).to receive(:read).with(key).and_call_original` for both `cache[key]` and `cache.read(key)` in RSpec.
       #
       def [](key)
         read(key)
       end
 
       ##
-      # https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-write
+      # @param key [Object] Can be any type.
+      # @param value [Object] Can be any type.
+      # @return [Object] Can be any type.
+      #
+      # @internal
+      #   https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-write
       #
       def write(key, value)
         hash[key] = value
       end
 
       ##
-      # NOTE: `alias_method` is NOT used in order to have an ability to use `allow(cache).to receive(:write).with(key, value).and_call_original` for both `cache[key] = value` and `cache.write(key, value)` in RSpec.
+      # @param key [Object] Can be any type.
+      # @param value [Object] Can be any type.
+      # @return [Object] Can be any type.
+      #
+      # @internal
+      #   NOTE: `alias_method` is NOT used in order to have an ability to use `allow(cache).to receive(:write).with(key, value).and_call_original` for both `cache[key] = value` and `cache.write(key, value)` in RSpec.
       #
       def []=(key, value)
         write(key, value)
       end
 
       ##
-      # https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-delete
+      # @param key [Object] Can be any type.
+      # @return [Object] Can be any type.
+      #
+      # @internal
+      #   https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-delete
       #
       def delete(key)
         hash.delete(key)
       end
 
       ##
-      # https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-fetch
+      # @param key [Object] Can be any type.
+      # @param block [Proc]
+      # @return [Object] Can be any type.
+      #
+      # @internal
+      #   https://api.rubyonrails.org/classes/ActiveSupport/Cache/Store.html#method-i-fetch
       #
       def fetch(key, &block)
         return read(key) unless block
 
         exist?(key) ? read(key) : write(key, block.call)
       end
+
+      ##
+      # @param other [Object] Can be any type.
+      # @return [Boolean]
+      #
+      def ==(other)
+        return unless other.instance_of?(self.class)
+
+        return false if hash != other.hash
+
+        true
+      end
+
+      protected
+
+      ##
+      # @!attribute [r] hash
+      #   @return [Hash]
+      #
+      attr_reader :hash
     end
   end
 end
