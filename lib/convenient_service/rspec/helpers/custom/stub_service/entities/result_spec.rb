24b72a9e38c6c812f54cb3c860f0774ea1ccc09d
Marian13 <mariankostyk13895@gmail.com>
2022-11-25 19:51:46 +0200

    refactor(stub_service): use create_without_initialize

--- lib/convenient_service/rspec/helpers/custom/stub_service/entities/result_spec.rb
+++ lib/convenient_service/rspec/helpers/custom/stub_service/entities/result_spec.rb
@@ -10,52 +10,97 @@ module ConvenientService
         class StubService < Support::Command
           module Entities
             class ResultSpec
+              ##
+              # @param status [Symbol]
+              # @param service_class [Class]
+              # @param chain [Hash]
+              # @return [void]
+              # @since 0.1.0
+              #
               def initialize(status:, service_class: nil, chain: {})
                 @status = status
                 @service_class = service_class
                 @chain = chain
               end
 
+              ##
+              # @param service_class [Class]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def for(service_class)
                 self.class.new(status: status, service_class: service_class, chain: chain)
               end
 
+              ##
+              # @param data [Hash]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def with_data(data)
                 chain[:data] = data
 
                 self
               end
 
+              ##
+              # @param message [String]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def with_message(message)
                 chain[:message] = message
 
                 self
               end
 
+              ##
+              # @param code [String]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def with_code(code)
                 chain[:code] = code
 
                 self
               end
 
+              ##
+              # @param data [Hash]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def and_data(data)
                 chain[:data] = data
 
                 self
               end
 
+              ##
+              # @param message [String]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def and_message(message)
                 chain[:message] = message
 
                 self
               end
 
+              ##
+              # @param code [String]
+              # @return [ConvenientService::RSpec::Helpers::Custom::StubService::Entities::ResultSpec]
+              # @since 0.1.0
+              #
               def and_code(code)
                 chain[:code] = code
 
                 self
               end
 
+              ##
+              # @return [Object]
+              #
               def calculate_value
                 ##
                 # TODO: Assert.
@@ -65,12 +110,34 @@ module ConvenientService
 
               private
 
-              attr_reader :status, :service_class, :chain
+              ##
+              # @!attribute [r]
+              #   @return [Symbol]
+              #
+              attr_reader :status
 
+              ##
+              # @!attribute [r]
+              #   @return [Class]
+              #
+              attr_reader :service_class
+
+              ##
+              # @!attribute [r]
+              #   @return [Hash]
+              #
+              attr_reader :chain
+
+              ##
+              # @return [Hash]
+              #
               def kwargs
                 @kwargs ||= calculate_kwargs
               end
 
+              ##
+              # @return [Hash]
+              #
               def calculate_kwargs
                 kwargs = {}
 
@@ -83,32 +150,53 @@ module ConvenientService
                 kwargs
               end
 
+              ##
+              # @return [Boolean]
+              #
               def used_data?
                 chain.key?(:data)
               end
 
+              ##
+              # @return [Boolean]
+              #
               def used_message?
                 chain.key?(:message)
               end
 
+              ##
+              # @return [Boolean]
+              #
               def used_code?
                 chain.key?(:code)
               end
 
+              ##
+              # @return [Hash]
+              #
               def data
                 @data ||= chain[:data] || {}
               end
 
+              ##
+              # @return [String]
+              #
               def message
                 @message ||= chain[:message] || ""
               end
 
+              ##
+              # @return [String]
+              #
               def code
                 @code ||= chain[:code] || ""
               end
 
+              ##
+              # @return [Object]
+              #
               def service_instance
-                @service_instance ||= service_class.new_without_initialize
+                @service_instance ||= service_class.create_without_initialize
               end
             end
           end
