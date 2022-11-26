303434ca9cd337cd69df9b0cf87a37db349a8b8b
Marian13 <mariankostyk13895@gmail.com>
2022-11-26 04:34:37 +0200

    docs(has_inspect): add more signatures

--- lib/convenient_service/service/plugins/has_result_steps/entities/step/plugins/has_inspect/concern.rb
+++ lib/convenient_service/service/plugins/has_result_steps/entities/step/plugins/has_inspect/concern.rb
@@ -14,6 +14,7 @@ module ConvenientService
                   instance_methods do
                     ##
                     # @return [String]
+                    # @since 0.2.0
                     #
                     def inspect
                       "<#{container.klass.name}::Step service: #{service.klass.name}>"
