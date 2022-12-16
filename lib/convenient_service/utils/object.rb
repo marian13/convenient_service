a5c30a1be835a66cdbaa21752d7404b2a5d50cbf
Marian13 <mariankostyk13895@gmail.com>
2022-12-16 22:23:25 +0200

    refactor(utils): introduce instance_variable_fetch

--- lib/convenient_service/utils/object.rb
+++ lib/convenient_service/utils/object.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
+require_relative "object/instance_variable_fetch"
 require_relative "object/resolve_type"
 
 module ConvenientService
   module Utils
     module Object
       class << self
+        ##
+        # @example
+        #   ConvenientService::Utils::Object.instance_variable_fetch("abc", :@foo) { :bar }
+        #
+        def instance_variable_fetch(...)
+          InstanceVariableFetch.call(...)
+        end
+
         ##
         # @example
         #   ConvenientService::Utils::Object.resolve_type("foo")
