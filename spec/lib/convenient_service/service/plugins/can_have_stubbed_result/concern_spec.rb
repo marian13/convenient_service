4aab013cfecfb3bedd8f4b2f3d09209e6d4d883a
Marian13 <mariankostyk13895@gmail.com>
2022-12-16 22:53:04 +0200

    test(can_have_stubbed_result): add stubbed_results specs

---
+++ spec/lib/convenient_service/service/plugins/can_have_stubbed_result/concern_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+RSpec.describe ConvenientService::Service::Plugins::CanHaveStubbedResult::Concern do
+  include ConvenientService::RSpec::Matchers::CacheItsValue
+
+  let(:service_class) do
+    Class.new.tap do |klass|
+      klass.class_exec(described_class) do |mod|
+        include mod
+      end
+    end
+  end
+
+  example_group "modules" do
+    include ConvenientService::RSpec::Matchers::IncludeModule
+    include ConvenientService::RSpec::Matchers::ExtendModule
+
+    subject { described_class }
+
+    it { is_expected.to include_module(ConvenientService::Support::Concern) }
+
+    context "when included" do
+      subject { service_class }
+
+      it { is_expected.to extend_module(described_class::ClassMethods) }
+    end
+  end
+
+  example_group "class methods" do
+    describe ".stubbed_results" do
+      it "returns cache scoped by self" do
+        expect(service_class.stubbed_results).to eq(ConvenientService::Support::Cache.new.scope(service_class))
+      end
+
+      specify { expect { service_class.stubbed_results }.to cache_its_value }
+    end
+  end
+end
