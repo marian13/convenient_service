d0ca4c80b4c0d69f8658dcc567ea7ccd04ffb16a
Marian13 <mariankostyk13895@gmail.com>
2022-12-17 20:37:20 +0200

    fix(example): fix rails_service/config_spec.rb

--- spec/lib/convenient_service/examples/rails/gemfile/rails_service/config_spec.rb
+++ spec/lib/convenient_service/examples/rails/gemfile/rails_service/config_spec.rb
@@ -27,6 +27,7 @@ RSpec.describe ConvenientService::Examples::Rails::Gemfile::RailsService::Config
       example_group "service" do
         let(:concerns) do
           [
+            ConvenientService::Service::Plugins::CanHaveStubbedResult::Concern,
             ConvenientService::Common::Plugins::HasInternals::Concern,
             ConvenientService::Common::Plugins::HasConstructor::Concern,
             ConvenientService::Plugins::Common::HasConstructorWithoutInitialize::Concern,
