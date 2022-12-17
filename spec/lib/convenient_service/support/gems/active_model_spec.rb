7b48a7d1fdeb10f3082e5b160774d7bdec8367fa
Marian13 <mariankostyk13895@gmail.com>
2022-12-17 04:38:18 +0200

    test(version): add specs when gem is not loaded

--- spec/lib/convenient_service/support/gems/active_model_spec.rb
+++ spec/lib/convenient_service/support/gems/active_model_spec.rb
@@ -14,6 +14,16 @@ RSpec.describe ConvenientService::Support::Gems::ActiveModel do
         end
       end
     end
+
+    context "when `ActiveModel` is NOT loaded" do
+      before do
+        allow(described_class).to receive(:loaded?).and_return(false)
+      end
+
+      it "return null version" do
+        expect(described_class.version).to be_instance_of(ConvenientService::Support::Version::NullVersion)
+      end
+    end
   end
 end
 # rubocop:enable RSpec/NestedGroups
--- spec/lib/convenient_service/support/gems/rspec_spec.rb
+++ spec/lib/convenient_service/support/gems/rspec_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe ConvenientService::Support::Gems::RSpec do
       it "return version" do
         expect(described_class.version).to eq(ConvenientService::Support::Version.new(RSpec::Core::Version::STRING))
       end
+
+      context "when `RSpec` is NOT loaded" do
+        before do
+          allow(described_class).to receive(:loaded?).and_return(false)
+        end
+
+        it "return null version" do
+          expect(described_class.version).to be_instance_of(ConvenientService::Support::Version::NullVersion)
+        end
+      end
     end
   end
 end
