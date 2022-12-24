e4b661a65861805d2fbac27f2d6977e7497d6dce
Marian13 <mariankostyk13895@gmail.com>
2022-12-24 20:43:52 +0200

    test(delegate_to): add specs for WithArguments

--- lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings/with_arguments.rb
+++ lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings/with_arguments.rb
@@ -7,100 +7,12 @@ module ConvenientService
         class DelegateTo
           module Entities
             module Chainings
-              class WithArguments
-                ##
-                # @internal
-                #   NOTE: `include ::RSpec::Mocks::ExampleMethods`.
-                #   - https://github.com/rspec/rspec-mocks/blob/v3.11.1/lib/rspec/mocks/example_methods.rb
-                #   - https://github.com/rspec/rspec-mocks/blob/main/lib/rspec/mocks/example_methods.rb
-                #
-                include ::RSpec::Mocks::ExampleMethods
-
-                include Support::Delegate
-
-                ##
-                # @!attribute [r] matcher
-                #   @return [ConvenientService::RSpec::Matchers::Custom::DelegateTo]
-                attr_reader :matcher
-
-                ##
-                # @return [Object]
-                #
-                attr_reader :block_expectation_value
-
-                ##
-                # @return [Boolean]
-                #
-                delegate :should_call_original?, to: :matcher
-
-                ##
-                # @return [Object]
-                #
-                delegate :object, to: :matcher
-
-                ##
-                # @return [String, Symbol]
-                #
-                delegate :method, to: :matcher
-
-                ##
-                # @return [Array]
-                #
-                delegate :expected_arguments, to: :matcher
-
-                ##
-                # @return [Array]
-                #
-                delegate :delegations, to: :matcher
-
-                ##
-                # @return [String]
-                #
-                delegate :printable_actual_arguments, to: :matcher
-
-                ##
-                # @return [String]
-                #
-                delegate :printable_block_expectation, to: :matcher
-
-                ##
-                # @return [String]
-                #
-                delegate :printable_method, to: :matcher
-
-                ##
-                # @param matcher [ConvenientService::RSpec::Matchers::Custom::DelegateTo]
-                # @return [void]
-                #
-                def initialize(matcher:)
-                  @matcher = matcher
-                end
-
+              class WithArguments < Chainings::Base
                 ##
                 # @return [void]
                 #
-                def apply_mocks!
-                  ##
-                  # NOTE: RSpec `allow(object).to receive(method).with(*args, **kwargs)` does NOT support block.
-                  # https://github.com/rspec/rspec-mocks/issues/1182#issuecomment-679820352
-                  #
-                  # NOTE: RSpec `allow(object).to receive(method) do` does NOT support `and_call_original`.
-                  # https://github.com/rspec/rspec-mocks/issues/774#issuecomment-54245277
-                  #
-                  # NOTE: That is why `and_wrap_original` is used.
-                  # https://relishapp.com/rspec/rspec-mocks/docs/configuring-responses/wrapping-the-original-implementation
-                  #
-                  allow(object).to receive(method).and_wrap_original do |original, *actual_args, **actual_kwargs, &actual_block|
-                    ##
-                    # TODO: Add backtrace for easier reason tracing.
-                    #
-                    delegations << Entities::Delegation.new(args: actual_args, kwargs: actual_kwargs, block: actual_block)
-
-                    ##
-                    # NOTE: Imitates `and_call_original`.
-                    #
-                    original.call(*actual_args, **actual_kwargs, &actual_block) if should_call_original?
-                  end
+                def apply_stubs!
+                  Commands::ApplyStubToTrackDelegations.call(matcher: matcher)
                 end
 
                 ##
@@ -108,31 +20,19 @@ module ConvenientService
                 # @return [Boolean]
                 #
                 def matches?(block_expectation_value)
-                  @block_expectation_value = block_expectation_value
+                  super
 
-                  ##
-                  # IMPORTANT: A matcher should always return a boolean.
-                  # https://github.com/zverok/saharspec/blob/master/lib/saharspec/matchers/send_message.rb#L59
-                  #
-                  # NOTE: RSpec raises exception when any `expect` is NOT satisfied.
-                  # So, this `true` is returned only when all `expect` are successful.
-                  #
-                  delegations.any? { |delegation| delegation.arguments == expected_arguments }
-                end
-
-                ##
-                # @param block_expectation_value [Object]
-                # @return [Boolean]
-                #
-                def does_not_match?(block_expectation_value)
-                  !matches?(block_expectation_value)
+                  matcher.delegations.any? { |delegation| delegation.arguments == matcher.expected_arguments }
                 end
 
                 ##
                 # @return [String]
                 #
+                # @internal
+                #   TODO: Prettier message.
+                #
                 def failure_message
-                  <<~MESSAGE
+                  <<~MESSAGE.chomp
                     #{failure_message_expected_part}
 
                     #{failure_message_got_part}
@@ -143,13 +43,13 @@ module ConvenientService
                 # @return [String]
                 #
                 def failure_message_when_negated
-                  "expected `#{printable_block_expectation}` NOT to delegate to `#{printable_method}` with `#{printable_expected_arguments}` at least once, but it did."
+                  "expected `#{matcher.printable_block_expectation}` NOT to delegate to `#{matcher.printable_method}` with `#{matcher.printable_expected_arguments}` at least once, but it did."
                 end
 
                 private
 
                 def failure_message_expected_part
-                  "expected `#{printable_block_expectation}` to delegate to `#{printable_method}` with `#{printable_expected_arguments}` at least once, but it didn't."
+                  "expected `#{matcher.printable_block_expectation}` to delegate to `#{matcher.printable_method}` with `#{matcher.printable_expected_arguments}` at least once, but it didn't."
                 end
 
                 ##
@@ -159,15 +59,11 @@ module ConvenientService
                 #   TODO: String diff.
                 #
                 def failure_message_got_part
-                  if delegations.empty?
-                    "got not delegated at all"
+                  if matcher.delegations.any?
+                    "got delegated with #{matcher.printable_actual_arguments}"
                   else
-                    "got delegated with #{printable_actual_arguments}"
+                    "got not delegated at all"
                   end
-                end
-
-                def printable_expected_arguments
-                  Commands::GeneratePrintableArguments.call(arguments: expected_arguments)
                 end
               end
             end
