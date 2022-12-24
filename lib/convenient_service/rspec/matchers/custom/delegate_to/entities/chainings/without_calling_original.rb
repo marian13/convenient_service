1a74131c0e7eaef8810993ef4a1d0a5bee587616
Marian13 <mariankostyk13895@gmail.com>
2022-12-24 18:48:58 +0200

    test(delegate_to): add spes for WithoutCallingOriginal

--- lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings/without_calling_original.rb
+++ lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings/without_calling_original.rb
@@ -7,75 +7,22 @@ module ConvenientService
         class DelegateTo
           module Entities
             module Chainings
-              class WithoutCallingOriginal
-                include Support::Delegate
-
-                ##
-                # @return [ConvenientService::RSpec::Matchers::Custom::DelegateTo]
-                #
-                attr_reader :matcher
-
-                ##
-                # @return [Object]
-                #
-                attr_reader :block_expectation_value
-
-                ##
-                # @return [Object]
-                #
-                delegate :object, to: :matcher
-
-                ##
-                # @param matcher [ConvenientService::RSpec::Matchers::Custom::DelegateTo]
-                # @return [void]
-                #
-                def initialize(matcher:)
-                  @matcher = matcher
-                end
-
-                ##
-                # @return [true]
-                #
-                def should_call_original?
-                  false
-                end
-
-                ##
-                # @return [void]
-                #
-                def apply_mocks!
-                end
-
+              class WithoutCallingOriginal < Chainings::Base
                 ##
                 # @param block_expectation_value [Object]
-                # @return [true]
+                # @return [Boolean]
                 #
                 def matches?(block_expectation_value)
-                  @block_expectation_value = block_expectation_value
+                  super
 
                   true
                 end
 
                 ##
-                # @param value [Object]
                 # @return [Boolean]
                 #
-                def does_not_match?(value)
-                  !matches?(value)
-                end
-
-                ##
-                # @return [String]
-                #
-                def failure_message
-                  ""
-                end
-
-                ##
-                # @return [String]
-                #
-                def failure_message_when_negated
-                  ""
+                def should_call_original?
+                  false
                 end
               end
             end
---
+++ spec/lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings/without_calling_original_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+# rubocop:disable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
+RSpec.describe ConvenientService::RSpec::Matchers::Custom::DelegateTo::Entities::Chainings::WithoutCallingOriginal do
+  let(:chaining) { described_class.new(matcher: matcher) }
+
+  let(:matcher) { ConvenientService::RSpec::Matchers::Custom::DelegateTo::Entities::Matcher.new(object, method) }
+
+  let(:klass) do
+    Class.new do
+      def foo
+        bar
+      end
+
+      def bar
+        "bar value"
+      end
+    end
+  end
+
+  let(:object) { klass.new }
+  let(:method) { :bar }
+  let(:block_expectation_value) { Object.new }
+
+  example_group "inheritance" do
+    include ConvenientService::RSpec::Matchers::BeDescendantOf
+
+    subject { described_class }
+
+    it { is_expected.to be_descendant_of(ConvenientService::RSpec::Matchers::Custom::DelegateTo::Entities::Chainings::Base) }
+  end
+
+  example_group "instance methods" do
+    describe "#matches?" do
+      ##
+      # NOTE: If it calls `super` then block_expectation_value is set.
+      #
+      it "calls `super`" do
+        chaining.matches?(block_expectation_value)
+
+        expect(chaining.block_expectation_value).to eq(block_expectation_value)
+      end
+
+      it "returns `true`" do
+        expect(chaining.matches?(block_expectation_value)).to eq(true)
+      end
+    end
+
+    describe "#should_call_original?" do
+      it "returns `false`" do
+        expect(chaining.should_call_original?).to eq(false)
+      end
+    end
+  end
+end
+# rubocop:enable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
