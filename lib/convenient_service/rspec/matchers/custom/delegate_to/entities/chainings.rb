62ac6185ac775104971a0bff3f72661cc528a22b
Marian13 <mariankostyk13895@gmail.com>
2022-12-24 02:52:43 +0200

    refactor(delegate_to): introduce base chaining

--- lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings.rb
+++ lib/convenient_service/rspec/matchers/custom/delegate_to/entities/chainings.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "chainings/base"
+
 require_relative "chainings/with_arguments"
 require_relative "chainings/without_arguments"
 require_relative "chainings/return_its_value"
