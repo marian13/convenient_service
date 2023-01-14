9f5980aba3d403e51263c5cbc2b977b139044461
Olha-30 <ovasia@softserveinc.com>
2023-01-15 00:58:32 +0200

    test(aliases): fix typo

--- spec/lib/convenient_service/aliases_spec.rb
+++ spec/lib/convenient_service/aliases_spec.rb
@@ -6,6 +6,6 @@ require "convenient_service"
 
 # rubocop:disable RSpec/DescribeClass
 RSpec.describe "convenient_service/aliases" do
-  specify { expect(ConvenientService::Concerns).to eq(ConvenientService::Support::Concern) }
+  specify { expect(ConvenientService::Concern).to eq(ConvenientService::Support::Concern) }
 end
 # rubocop:enable RSpec/DescribeClass
