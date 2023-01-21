4097d03ba60abf0be0db33c0a6042363cef65e7f
Marian13 <mariankostyk13895@gmail.com>
2023-01-21 20:10:57 +0200

    chore(dependency_container): remove redundant method

--- lib/convenient_service/support/dependency_container/entities/namespace_collection.rb
+++ lib/convenient_service/support/dependency_container/entities/namespace_collection.rb
@@ -14,15 +14,6 @@ module ConvenientService
           end
 
           ##
-          #
-          #
-          def define_namespace(namespace)
-            namespaces << namespace
-
-            define_singleton_method(namespace.name) { namespace.body.call }
-          end
-
-          ##
           # @param name [String, Symbol]
           # @return [ConvenientService::Support::DependencyContainer::Entities::Namespace, nil]
           #
