0b93419ac95f22836fcc177acc340e1b8eecbecf
Olha-30 <ovasia@softserveinc.com>
2023-01-31 22:40:48 +0200

    feat(export_matcher): create matcher for dependency_containers export

--- lib/convenient_service/rspec/matchers/custom/export.rb
+++ lib/convenient_service/rspec/matchers/custom/export.rb
@@ -1,58 +1,36 @@
 # frozen_string_literal: true
 
-require_relative "export/entities"
-
 module ConvenientService
   module RSpec
     module Matchers
       module Custom
         class Export
-          attr_reader :method_name, :scope
-
           def initialize(method_name, scope: :instance)
             @method_name = method_name
             @scope = scope
           end
 
           def matches?(container)
-            method = container.exported_methods.find_by(full_name: method_name, scope: scope)
+            @container = container
 
-            return false unless method
-
-            return check_expected_value(container: container, method: method_name, scope: scope) if @expected_value
-
-            true
+            Utils::Bool.to_bool(container.exported_methods.find_by(full_name: method_name, scope: scope))
           end
 
           def description
-            "export `#{method_name}` with `#{scope}` scope"
+            "export `#{method_name}` with scope `#{scope}`"
           end
 
           def failure_message
-            "Failure message"
+            "expected `#{container.class}` to have export `#{method_name}` with scope `#{scope}`"
           end
 
-          def and_return(expected_value)
-            @expected_value = expected_value
-
-            self
+          def failure_message_when_negated
+            "expected `#{container.class}` NOT to have export `#{method_name}` with scope `#{scope}`"
           end
 
           private
 
-          def check_expected_value(container:, method:, scope: :instance)
-            klass = Class.new do
-              include ConvenientService::DependencyContainer::Import
-
-              @method_obj = import method, scope: scope, from: container
-
-              def self.matches_value?(expected_value)
-                @method_obj.body.call == expected_value
-              end
-            end
-
-            klass.matches_value?(@expected_value)
-          end
+          attr_reader :method_name, :scope, :container
         end
       end
     end
--- lib/convenient_service/rspec/matchers/custom/export/entities/chainings.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "chainings/sub_matchers/and_return"
