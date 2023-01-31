bbba1b2c662c72519a36d65dd153e03ab167a8f6
Olha-30 <ovasia@softserveinc.com>
2023-01-31 23:06:48 +0200

    docs(export_matcher): add signatures

--- lib/convenient_service/rspec/matchers/custom/export.rb
+++ lib/convenient_service/rspec/matchers/custom/export.rb
@@ -5,32 +5,66 @@ module ConvenientService
     module Matchers
       module Custom
         class Export
+          ##
+          # @param method_name [Symbol, String]
+          # @param scope [Symbol]
+          # @return [void]
+          #
           def initialize(method_name, scope: :instance)
             @method_name = method_name
             @scope = scope
           end
 
+          ##
+          # @param container [Class, Module]
+          # @return [Boolean]
+          #
           def matches?(container)
             @container = container
 
             Utils::Bool.to_bool(container.exported_methods.find_by(full_name: method_name, scope: scope))
           end
 
+          ##
+          # @return [String]
+          #
           def description
             "export `#{method_name}` with scope `#{scope}`"
           end
 
+          ##
+          # @return [String]
+          #
           def failure_message
             "expected `#{container.class}` to have export `#{method_name}` with scope `#{scope}`"
           end
 
+          ##
+          # @return [String]
+          #
           def failure_message_when_negated
             "expected `#{container.class}` NOT to have export `#{method_name}` with scope `#{scope}`"
           end
 
           private
 
-          attr_reader :method_name, :scope, :container
+          ##
+          # @!attribute [r] method_name
+          #   @return [Symbol, String]
+          #
+          attr_reader :method_name
+
+          ##
+          # @!attribute [r] scope
+          #   @return [Symbol]
+          #
+          attr_reader :scope
+
+          ##
+          # @!attribute [r] container
+          #   @return [Class, Module]
+          #
+          attr_reader :container
         end
       end
     end
