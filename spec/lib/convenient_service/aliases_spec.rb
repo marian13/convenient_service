6abcb1ad481cad66eba2d4d409e3ce16b0f6591d
Marian13 <mariankostyk13895@gmail.com>
2023-02-05 15:46:22 +0200

    feat(command): expose command

--- spec/lib/convenient_service/aliases_spec.rb
+++ spec/lib/convenient_service/aliases_spec.rb
@@ -6,6 +6,7 @@ require "convenient_service"
 
 # rubocop:disable RSpec/DescribeClass
 RSpec.describe "convenient_service/aliases" do
+  specify { expect(ConvenientService::Command).to eq(ConvenientService::Support::Command) }
   specify { expect(ConvenientService::Concern).to eq(ConvenientService::Support::Concern) }
   specify { expect(ConvenientService::DependencyContainer).to eq(ConvenientService::Support::DependencyContainer) }
 end
