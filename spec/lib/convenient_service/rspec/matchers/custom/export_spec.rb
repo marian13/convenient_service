ea938a5d20d9da9f0a47d6804dffa77ae89cab50
Olha-30 <ovasia@softserveinc.com>
2023-02-15 11:29:30 +0200

    style(export_matcher): remove context

--- spec/lib/convenient_service/rspec/matchers/custom/export_spec.rb
+++ spec/lib/convenient_service/rspec/matchers/custom/export_spec.rb
@@ -40,90 +40,84 @@ RSpec.describe ConvenientService::RSpec::Matchers::Custom::Export do
 
   example_group "instance methods" do
     describe "#matches?" do
-      context "when container" do
-        context "when container can NOT export methods" do
-          let(:container) { Module.new }
+      context "when container can NOT export methods" do
+        let(:container) { Module.new }
 
-          let(:error_message) do
-            <<~TEXT
-              Module `#{container}` can NOT export methods.
+        let(:error_message) do
+          <<~TEXT
+            Module `#{container}` can NOT export methods.
 
-              Did you forget to include `ConvenientService::DependencyContainer::Export` into it?
-            TEXT
-          end
-
-          it "raises `ConvenientService::Support::DependencyContainer::Errors::NotExportableModule`" do
-            expect { matcher_result }
-              .to raise_error(ConvenientService::Support::DependencyContainer::Errors::NotExportableModule)
-              .with_message(error_message)
-          end
+            Did you forget to include `ConvenientService::DependencyContainer::Export` into it?
+          TEXT
         end
 
-        context "when container can export methods" do
-          it "does NOT raise" do
-            expect { matcher_result }
-              .not_to raise_error(ConvenientService::Support::DependencyContainer::Errors::NotExportableModule)
-          end
+        it "raises `ConvenientService::Support::DependencyContainer::Errors::NotExportableModule`" do
+          expect { matcher_result }
+            .to raise_error(ConvenientService::Support::DependencyContainer::Errors::NotExportableModule)
+            .with_message(error_message)
         end
       end
 
-      context "when scope" do
-        context "when scope is NOT passed" do
-          let(:full_name) { :foo }
-          let(:kwargs) { ConvenientService::Utils::Hash.except(default_kwargs, [:scope]) }
-
-          it "defaults scope to :instance" do
-            expect(matcher_result).to eq(true)
-          end
-        end
-
-        context "when scope is passed, but NOT valid" do
-          let(:full_name) { :foo }
-          let(:scope) { :non_existent_scope }
-
-          let(:error_message) do
-            <<~TEXT
-              Scope `#{scope.inspect}` is NOT valid.
-
-              Valid options are `:instance`, `:class`.
-            TEXT
-          end
-
-          it "raises `DependencyContainer::Errors::NotExportableModule`" do
-            expect { matcher_result }
-              .to raise_error(ConvenientService::Support::DependencyContainer::Errors::InvalidScope)
-              .with_message(error_message)
-          end
-        end
-
-        context "when scope is passed and valid" do
-          let(:full_name) { :bar }
-          let(:scope) { :class }
-
-          it "does NOT raise" do
-            expect { matcher_result }
-              .not_to raise_error(ConvenientService::Support::DependencyContainer::Errors::InvalidScope)
-          end
+      context "when container can export methods" do
+        it "does NOT raise" do
+          expect { matcher_result }
+            .not_to raise_error(ConvenientService::Support::DependencyContainer::Errors::NotExportableModule)
         end
       end
 
-      context "when method" do
-        context "when method is NOT exported" do
-          let(:full_name) { :non_existent }
-          let(:scope) { :class }
+      context "when scope is NOT passed" do
+        let(:full_name) { :foo }
+        let(:kwargs) { ConvenientService::Utils::Hash.except(default_kwargs, [:scope]) }
 
-          it "returns false" do
-            expect(matcher_result).to eq(false)
-          end
+        it "defaults scope to :instance" do
+          expect(matcher_result).to eq(true)
+        end
+      end
+
+      context "when scope is passed, but NOT valid" do
+        let(:full_name) { :foo }
+        let(:scope) { :non_existent_scope }
+
+        let(:error_message) do
+          <<~TEXT
+            Scope `#{scope.inspect}` is NOT valid.
+
+            Valid options are `:instance`, `:class`.
+          TEXT
         end
 
-        context "when method is exported" do
-          let(:full_name) { :bar }
-          let(:scope) { :class }
+        it "raises `DependencyContainer::Errors::NotExportableModule`" do
+          expect { matcher_result }
+            .to raise_error(ConvenientService::Support::DependencyContainer::Errors::InvalidScope)
+            .with_message(error_message)
+        end
+      end
 
-          it "returns true" do
-            expect(matcher_result).to eq(true)
-          end
+      context "when scope is passed and valid" do
+        let(:full_name) { :bar }
+        let(:scope) { :class }
+
+        it "does NOT raise" do
+          expect { matcher_result }
+            .not_to raise_error(ConvenientService::Support::DependencyContainer::Errors::InvalidScope)
+        end
+      end
+
+      context "when method is NOT exported" do
+        let(:full_name) { :non_existent }
+        let(:scope) { :class }
+
+        it "returns false" do
+          expect(matcher_result).to eq(false)
+        end
+      end
+
+      context "when method is exported" do
+        let(:full_name) { :bar }
+        let(:scope) { :class }
+
+        it "returns true" do
+          expect(matcher_result).to eq(true)
         end
       end
     end
