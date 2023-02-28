c2c33ab3a7c8c83c2cfcc2f43da0c7c81ca4764d
Olha-30 <ovasia@softserveinc.com>
2023-02-28 19:46:01 +0200

    test(assert_valid_container): add specs

---
+++ spec/lib/convenient_service/support/dependency_container/commands/assert_valid_container_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+RSpec.describe ConvenientService::Support::DependencyContainer::Commands::AssertValidContainer do
+  example_group "class methods" do
+    describe ".call" do
+      subject(:command_result) { described_class.call(from: container) }
+
+      context "when `container` is NOT valid" do
+        let(:container) { Module.new }
+
+        let(:error_message) do
+          <<~TEXT
+            Module `#{container}` can NOT export methods.
+
+            Did you forget to include `ConvenientService::DependencyContainer::Export` into it?
+          TEXT
+        end
+
+        it "raises `ConvenientService::Support::DependencyContainer::Errors::NotExportableModule`" do
+          expect { command_result }
+            .to raise_error(ConvenientService::Support::DependencyContainer::Errors::NotExportableModule)
+            .with_message(error_message)
+        end
+      end
+
+      context "when `container` is valid" do
+        let(:container) do
+          Module.new do
+            include ConvenientService::DependencyContainer::Export
+          end
+        end
+
+        it "does NOT raise" do
+          expect { command_result }.not_to raise_error
+        end
+      end
+    end
+  end
+end
