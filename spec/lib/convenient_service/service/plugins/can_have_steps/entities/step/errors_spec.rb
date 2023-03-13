4fad2b7c5626174fa981a449e5ce664af7513c81
Marian13 <mariankostyk13895@gmail.com>
2023-03-13 14:30:57 +0200

    test(can_have_steps): add specs for errors

---
+++ spec/lib/convenient_service/service/plugins/can_have_steps/entities/step/errors_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+RSpec.describe ConvenientService::Service::Plugins::CanHaveSteps::Errors do
+  include ConvenientService::RSpec::Matchers::BeDescendantOf
+
+  specify { expect(described_class::StepHasNoOrganizer).to be_descendant_of(ConvenientService::Error) }
+end
