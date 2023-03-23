a21d5492982ce4c651922c92eb9e726a2ba618c0
Marian13 <mariankostyk13895@gmail.com>
2023-03-24 00:48:03 +0200

    refactor(plugins): restucture aliases

---
+++ spec/lib/convenient_service/service/plugins/can_have_steps/entities/step/plugins/aliases_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+# rubocop:disable RSpec/DescribeClass
+RSpec.describe "convenient_service/service/plugins/can_have_steps/entities/step/plugins/aliases" do
+  specify { expect(ConvenientService::Plugins::Step).to eq(ConvenientService::Service::Plugins::CanHaveSteps::Entities::Step::Plugins) }
+end
+# rubocop:enable RSpec/DescribeClass
