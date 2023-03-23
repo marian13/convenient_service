b37d735c03b08e57d930c9d5cb1c7cd089a07e0a
Marian13 <mariankostyk13895@gmail.com>
2023-03-23 20:11:16 +0200

    feat(support): safe_method

---
+++ lib/convenient_service/support/safe_method.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module ConvenientService
+  module Support
+    class SafeMethod
+      ##
+      # @!attribute [r] object
+      #   @return [Object] Can be any type.
+      #
+      attr_reader :object
+
+      ##
+      # @!attribute [r] method
+      #   @return [Symbol, String]
+      #
+      attr_reader :method
+
+      ##
+      # @!attribute [r] default
+      #   @return [Object] Can be any type.
+      #
+      attr_reader :default
+
+      ##
+      # @param object [Object] Can be any type.
+      # @param method [Symbol, String]
+      # @param default [Object, nil] Can be any type.
+      # @return [void]
+      #
+      def initialize(object, method, default: nil)
+        @object = object
+        @method = method
+        @default = default
+      end
+
+      ##
+      # @param args [Array<Object>]
+      # @param kwargs [Hash<Symbol, Object>]
+      # @param block [Proc, nil]
+      # @return [Object] Can be any type.
+      #
+      def call(*args, **kwargs, &block)
+        return default unless method_defined?
+
+        object.__send__(method, *args, **kwargs, &block)
+      end
+
+      private
+
+      ##
+      # @return [Boolean]
+      #
+      def method_defined?
+        Utils::Method.defined?(method, klass, private: true)
+      end
+
+      ##
+      # @return [Class]
+      #
+      def klass
+        @klass ||= Utils::Object.resolve_class(object)
+      end
+    end
+  end
+end
