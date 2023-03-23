78d009c4a497e2afe0247e24c1d0c9f37fe46265
Marian13 <mariankostyk13895@gmail.com>
2023-03-24 00:59:29 +0200

    test(configs): add jsend attributes and status to minimal config

--- spec/lib/convenient_service/configs/standard_spec.rb
+++ spec/lib/convenient_service/configs/standard_spec.rb
@@ -341,6 +341,46 @@ RSpec.describe ConvenientService::Configs::Standard do
             end
           end
 
+          example_group "service result data" do
+            example_group "concerns" do
+              let(:concerns) { [] }
+
+              it "sets service result data concerns" do
+                expect(service_class::Result::Data.concerns.to_a).to eq(concerns)
+              end
+            end
+          end
+
+          example_group "service result message" do
+            example_group "concerns" do
+              let(:concerns) { [] }
+
+              it "sets service result message concerns" do
+                expect(service_class::Result::Message.concerns.to_a).to eq(concerns)
+              end
+            end
+          end
+
+          example_group "service result code" do
+            example_group "concerns" do
+              let(:concerns) { [] }
+
+              it "sets service result code concerns" do
+                expect(service_class::Result::Code.concerns.to_a).to eq(concerns)
+              end
+            end
+          end
+
+          example_group "service result status" do
+            example_group "concerns" do
+              let(:concerns) { [] }
+
+              it "sets service result status concerns" do
+                expect(service_class::Result::Status.concerns.to_a).to eq(concerns)
+              end
+            end
+          end
+
           example_group "service result internals" do
             example_group "concerns" do
               let(:concerns) do
