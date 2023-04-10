45b9e249052e154123e1a014a16df0b2b165268b
Marian13 <mariankostyk13895@gmail.com>
2023-04-11 02:53:48 +0300

    feat(can_have_stubbed_results): use thread safe array backend

--- lib/convenient_service/service/plugins/can_have_stubbed_result/concern.rb
+++ lib/convenient_service/service/plugins/can_have_stubbed_result/concern.rb
@@ -44,9 +44,9 @@ module ConvenientService
               #
               cache =
                 if Support::Gems::RSpec.current_example
-                  Utils::Object.instance_variable_fetch(Support::Gems::RSpec.current_example, :@__convenient_service_stubbed_results__) { Support::Cache.create(backend: :array) }
+                  Utils::Object.instance_variable_fetch(Support::Gems::RSpec.current_example, :@__convenient_service_stubbed_results__) { Support::Cache.create(backend: :thread_safe_array) }
                 else
-                  Support::Cache.create(backend: :array)
+                  Support::Cache.create(backend: :thread_safe_array)
                 end
 
               cache.scope(self)
