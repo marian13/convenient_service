10822ff60dcfb4ae34417b359b4cdf736b329a27
Marian13 <mariankostyk13895@gmail.com>
2023-04-10 15:37:35 +0300

    chore(coverage): enable coverage for minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 ##
+# Makes sure that internal `ENV` variables are available.
+#
+require_relative "../env"
+
+##
+# Configures coverage tracking.
+#
+require_relative "coverage_helper"
+
+##
+# Allows to require gems listed in `Gemfile` and `gemspec`.
+#
+require "bundler/setup"
+
+##
 # TODO: More docs for `autorun`.
 # - https://github.com/minitest/minitest
 # - https://minitest.rubystyle.guide
@@ -13,7 +28,3 @@ require "minitest/autorun"
 # - https://github.com/thoughtbot/shoulda-context
 #
 require "shoulda-context"
-
-##
-# TODO: Set up `simplecov`.
-##
