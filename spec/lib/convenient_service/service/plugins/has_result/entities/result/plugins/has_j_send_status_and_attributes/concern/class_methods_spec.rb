ac907123290bc72adafe473328e20ac573b1f464
Marian13 <mariankostyk13895@gmail.com>
2023-05-01 21:59:25 +0300

    test(has_j_send_status_and_attributes): add specs for .code, .data, .message, .status

--- spec/lib/convenient_service/service/plugins/has_result/entities/result/plugins/has_j_send_status_and_attributes/concern/class_methods_spec.rb
+++ spec/lib/convenient_service/service/plugins/has_result/entities/result/plugins/has_j_send_status_and_attributes/concern/class_methods_spec.rb
@@ -12,12 +12,98 @@ RSpec.describe ConvenientService::Service::Plugins::HasResult::Entities::Result:
   let(:service_class) do
     Class.new do
       include ConvenientService::Configs::Standard
+
+      def result
+        success
+      end
     end
   end
 
   let(:result_class) { service_class.result_class }
+  let(:result_instance) { service_class.result }
+
+  let(:code) { result_instance.unsafe_code }
+  let(:data) { result_instance.unsafe_data }
+  let(:message) { result_instance.unsafe_message }
+  let(:status) { result_instance.status }
 
   example_group "class methods" do
+    describe ".code" do
+      let(:value) { :foo }
+
+      specify do
+        expect { result_class.code(value: value, result: result_instance) }
+          .to delegate_to(result_class.code_class, :cast!)
+          .with_arguments(value)
+      end
+
+      specify do
+        allow(result_class.code_class).to receive(:cast!).and_return(code)
+
+        expect { result_class.code(value: value, result: result_instance) }
+          .to delegate_to(code, :copy)
+          .with_arguments(overrides: {kwargs: {result: result_instance}})
+          .and_return_its_value
+      end
+    end
+
+    describe ".data" do
+      let(:value) { {foo: :bar} }
+
+      specify do
+        expect { result_class.data(value: value, result: result_instance) }
+          .to delegate_to(result_class.data_class, :cast!)
+          .with_arguments(value)
+      end
+
+      specify do
+        allow(result_class.data_class).to receive(:cast!).and_return(data)
+
+        expect { result_class.data(value: value, result: result_instance) }
+          .to delegate_to(data, :copy)
+          .with_arguments(overrides: {kwargs: {result: result_instance}})
+          .and_return_its_value
+      end
+    end
+
+    describe ".message" do
+      let(:value) { "foo" }
+
+      specify do
+        expect { result_class.message(value: value, result: result_instance) }
+          .to delegate_to(result_class.message_class, :cast!)
+          .with_arguments(value)
+      end
+
+      specify do
+        allow(result_class.message_class).to receive(:cast!).and_return(message)
+
+        expect { result_class.message(value: value, result: result_instance) }
+          .to delegate_to(message, :copy)
+          .with_arguments(overrides: {kwargs: {result: result_instance}})
+          .and_return_its_value
+      end
+    end
+
+    describe ".status" do
+      let(:value) { :foo }
+
+      specify do
+        expect { result_class.status(value: value, result: result_instance) }
+          .to delegate_to(result_class.status_class, :cast!)
+          .with_arguments(value)
+      end
+
+      specify do
+        allow(result_class.status_class).to receive(:cast!).and_return(status)
+
+        expect { result_class.status(value: value, result: result_instance) }
+          .to delegate_to(status, :copy)
+          .with_arguments(overrides: {kwargs: {result: result_instance}})
+          .and_return_its_value
+      end
+    end
+
     describe ".code_class" do
       specify do
         expect { result_class.code_class }
