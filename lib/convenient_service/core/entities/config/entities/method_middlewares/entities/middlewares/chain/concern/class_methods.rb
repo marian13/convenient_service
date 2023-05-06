8406823aaf0c583d144d79e30e335259285f68df
Marian13 <mariankostyk13895@gmail.com>
2023-05-06 21:19:38 +0300

    refactor(core): extract Chain Concern

---
+++ lib/convenient_service/core/entities/config/entities/method_middlewares/entities/middlewares/chain/concern/class_methods.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module ConvenientService
+  module Core
+    module Entities
+      class Config
+        module Entities
+          class MethodMiddlewares
+            module Entities
+              module Middlewares
+                class Chain < Middlewares::Base
+                  module Concern
+                    module ClassMethods
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/convenient_service/core/entities/config/entities/method_middlewares/entities/middlewares/chain/concern_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+# rubocop:disable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
+RSpec.describe ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::Middlewares::Chain::Concern do
+  example_group "modules" do
+    include ConvenientService::RSpec::Matchers::IncludeModule
+    include ConvenientService::RSpec::Matchers::ExtendModule
+
+    subject { described_class }
+
+    it { is_expected.to include_module(ConvenientService::Support::Concern) }
+
+    context "when included" do
+      subject { middleware_class }
+
+      let(:middleware_class) do
+        Class.new.tap do |klass|
+          klass.class_exec(described_class) do |mod|
+            include mod
+          end
+        end
+      end
+
+      it { is_expected.to include_module(described_class::InstanceMethods) }
+      it { is_expected.to extend_module(described_class::ClassMethods) }
+    end
+  end
+end
+# rubocop:enable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
