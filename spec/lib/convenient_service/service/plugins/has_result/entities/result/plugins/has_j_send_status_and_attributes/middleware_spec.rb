b9319ea5a3eeb81ee40d4b3c6b3e6e220c5bc14a
Marian13 <mariankostyk13895@gmail.com>
2023-05-11 20:48:35 +0300

    refactor(has_j_send_status_and_attributes): observe middleware

--- spec/lib/convenient_service/service/plugins/has_result/entities/result/plugins/has_j_send_status_and_attributes/middleware_spec.rb
+++ spec/lib/convenient_service/service/plugins/has_result/entities/result/plugins/has_j_send_status_and_attributes/middleware_spec.rb
@@ -6,10 +6,12 @@ require "convenient_service"
 
 # rubocop:disable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
 RSpec.describe ConvenientService::Service::Plugins::HasResult::Entities::Result::Plugins::HasJSendStatusAndAttributes::Middleware do
+  let(:middleware) { described_class }
+
   example_group "inheritance" do
     include ConvenientService::RSpec::Matchers::BeDescendantOf
 
-    subject { described_class }
+    subject { middleware }
 
     it { is_expected.to be_descendant_of(ConvenientService::MethodChainMiddleware) }
   end
@@ -23,7 +25,7 @@ RSpec.describe ConvenientService::Service::Plugins::HasResult::Entities::Result:
       end
 
       it "returns intended methods" do
-        expect(described_class.intended_methods).to eq(spec.intended_methods)
+        expect(middleware.intended_methods).to eq(spec.intended_methods)
       end
     end
   end
@@ -36,7 +38,7 @@ RSpec.describe ConvenientService::Service::Plugins::HasResult::Entities::Result:
 
       subject(:method_value) { method.call(**kwargs) }
 
-      let(:method) { wrap_method(result, :initialize, middlewares: described_class) }
+      let(:method) { wrap_method(result, :initialize, middleware: middleware) }
 
       let(:kwargs) do
         {
@@ -49,11 +51,19 @@ RSpec.describe ConvenientService::Service::Plugins::HasResult::Entities::Result:
       end
 
       let(:service) do
-        Class.new do
-          include ConvenientService::Configs::Minimal
+        Class.new.tap do |klass|
+          klass.class_exec(middleware) do |middleware|
+            include ConvenientService::Configs::Minimal
 
-          def result
-            success
+            self::Result.class_exec(middleware) do |middleware|
+              middlewares :initialize do
+                observe middleware
+              end
+            end
+
+            def result
+              success
+            end
           end
         end
       end
