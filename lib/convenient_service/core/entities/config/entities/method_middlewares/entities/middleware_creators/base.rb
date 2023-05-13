0ec1f9d0080efa4599b8f5337241628970535d37
Marian13 <mariankostyk13895@gmail.com>
2023-05-13 20:29:38 +0300

    refactor(core): extract base middleware creator

---
+++ lib/convenient_service/core/entities/config/entities/method_middlewares/entities/middleware_creators/base.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+module ConvenientService
+  module Core
+    module Entities
+      class Config
+        module Entities
+          class MethodMiddlewares
+            module Entities
+              module MiddlewareCreators
+                class Base
+                  include Support::Delegate
+
+                  ##
+                  # @!attribute [r] middleware
+                  #   @return [ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::Middlewares::Base]
+                  #
+                  attr_reader :middleware
+
+                  ##
+                  # @return [ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::Middlewares::Base]
+                  #
+                  delegate :to_observable_middleware, to: :middleware
+
+                  ##
+                  # @return [void]
+                  #
+                  # @internal
+                  #   TODO: Verbose exception.
+                  #
+                  def initialize(**kwargs)
+                    @middleware = kwargs.fetch(:middleware)
+                  end
+
+                  ##
+                  # @return [ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::MiddlewareCreators::With]
+                  #
+                  def with(...)
+                    Entities::MiddlewareCreators::With.new(middleware: self, arguments: Support::Arguments.new(...))
+                  end
+
+                  ##
+                  # @return [ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::MiddlewareCreators::Observable]
+                  #
+                  def observable
+                    Entities::MiddlewareCreators::Observable.new(middleware: self)
+                  end
+
+                  ##
+                  # @param stack [#call<Hash>]
+                  # @param kwargs [Hash{Symbol => Object}]
+                  # @return [ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::Middlewares::Base]
+                  #
+                  def new(stack, **kwargs)
+                    decorated_middleware.new(stack, **[extra_kwargs, middleware.extra_kwargs, kwargs].reduce(:merge))
+                  end
+
+                  ##
+                  # @return [ConvenientService::Core::Entities::Config::Entities::MethodMiddlewares::Entities::Middlewares::Base]
+                  #
+                  # @note May be overridden by descendants.
+                  #
+                  def decorated_middleware
+                    middleware
+                  end
+
+                  ##
+                  # @return [Hash{Symbol => Object}]
+                  #
+                  # @note May be overridden by descendants.
+                  #
+                  def extra_kwargs
+                    {}
+                  end
+
+                  ##
+                  # @param other [Object]
+                  # @return [Boolean, nil]
+                  #
+                  def ==(other)
+                    return unless other.instance_of?(self.class)
+
+                    return false if middleware != other.middleware
+
+                    true
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
