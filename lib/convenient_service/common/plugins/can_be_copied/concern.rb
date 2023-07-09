94e46fe7100964a4340608234f606e279ea516c0
Marian13 <mariankostyk13895@gmail.com>
2023-07-09 15:54:09 +0300

    test(can_be_copied): use delegate_to

--- lib/convenient_service/common/plugins/can_be_copied/concern.rb
+++ lib/convenient_service/common/plugins/can_be_copied/concern.rb
@@ -8,22 +8,39 @@ module ConvenientService
           include Support::Concern
 
           ##
-          # NOTE: `CanBeCopied` plugin expects that `CachesConstructorArguments` plugin is already used.
-          # That is why `constructor_arguments` method is available.
+          # @internal
+          #   NOTE: `CanBeCopied` plugin expects that `CachesConstructorArguments` plugin is already used.
+          #   That is why `constructor_arguments` method is available.
           #
           instance_methods do
             include Support::Copyable
 
+            ##
+            # @return [Array<Object>]
+            #
             def to_args
               constructor_arguments.args
             end
 
+            ##
+            # @return [Hash{Symbol => Object}]
+            #
             def to_kwargs
               constructor_arguments.kwargs
             end
 
+            ##
+            # @return [Proc, nil]
+            #
             def to_block
               constructor_arguments.block
+            end
+
+            ##
+            # @return [ConvenientService::Support::Arguments]
+            #
+            def to_arguments
+              constructor_arguments
             end
           end
         end
--- spec/lib/convenient_service/common/plugins/can_be_copied/concern_spec.rb
+++ spec/lib/convenient_service/common/plugins/can_be_copied/concern_spec.rb
@@ -4,6 +4,7 @@ require "spec_helper"
 
 require "convenient_service"
 
+# rubocop:disable RSpec/MultipleMemoizedHelpers
 RSpec.describe ConvenientService::Common::Plugins::CanBeCopied::Concern do
   example_group "modules" do
     include ConvenientService::RSpec::Matchers::IncludeModule
@@ -26,4 +27,58 @@ RSpec.describe ConvenientService::Common::Plugins::CanBeCopied::Concern do
       it { is_expected.to include_module(described_class::InstanceMethods) }
     end
   end
+
+  example_group "instance methods" do
+    include ConvenientService::RSpec::Matchers::DelegateTo
+
+    let(:service_class) do
+      Class.new do
+        include ConvenientService::Configs::Standard
+      end
+    end
+
+    let(:service_instance) { service_class.new(*args, **kwargs, &block) }
+    let(:constructor_arguments) { service_instance.constructor_arguments }
+
+    let(:args) { :foo }
+    let(:kwargs) { {foo: :bar} }
+    let(:block) { proc { :foo } }
+
+    describe "#to_args" do
+      specify do
+        expect { service_instance.to_args }
+          .to delegate_to(service_instance.constructor_arguments, :args)
+          .without_arguments
+          .and_return_its_value
+      end
+    end
+
+    describe "#to_kwargs" do
+      specify do
+        expect { service_instance.to_kwargs }
+          .to delegate_to(service_instance.constructor_arguments, :kwargs)
+          .without_arguments
+          .and_return_its_value
+      end
+    end
+
+    describe "#to_block" do
+      specify do
+        expect { service_instance.to_block }
+          .to delegate_to(service_instance.constructor_arguments, :block)
+          .without_arguments
+          .and_return_its_value
+      end
+    end
+
+    describe "to_arguments" do
+      specify do
+        expect { service_instance.to_arguments }
+          .to delegate_to(service_instance, :constructor_arguments)
+          .without_arguments
+          .and_return_its_value
+      end
+    end
+  end
 end
+# rubocop:enable RSpec/MultipleMemoizedHelpers
