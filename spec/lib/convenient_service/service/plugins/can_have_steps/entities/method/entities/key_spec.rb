a72b5c9f68d307738009a0ef4f8a33c174d9621d
Marian13 <mariankostyk13895@gmail.com>
2023-07-09 16:30:55 +0300

    refactor(can_have_steps): use to_arguments

--- spec/lib/convenient_service/service/plugins/can_have_steps/entities/method/entities/key_spec.rb
+++ spec/lib/convenient_service/service/plugins/can_have_steps/entities/method/entities/key_spec.rb
@@ -19,6 +19,8 @@ RSpec.describe ConvenientService::Service::Plugins::CanHaveSteps::Entities::Meth
   end
 
   example_group "instance methods" do
+    include ConvenientService::RSpec::Matchers::DelegateTo
+
     example_group "attributes" do
       include ConvenientService::RSpec::Matchers::HaveAttrReader
 
@@ -67,11 +69,25 @@ RSpec.describe ConvenientService::Service::Plugins::CanHaveSteps::Entities::Meth
       end
     end
 
-    describe "#to_args" do
-      let(:args_representation) { [value] }
+    example_group "conversions" do
+      let(:arguments) { ConvenientService::Support::Arguments.new(*args) }
+      let(:args) { [value] }
 
-      it "returns args representation of key" do
-        expect(key.to_args).to eq(args_representation)
+      describe "#to_args" do
+        specify do
+          allow(key).to receive(:to_arguments).and_return(arguments)
+
+          expect { key.to_args }
+            .to delegate_to(key.to_arguments, :args)
+            .without_arguments
+            .and_return_its_value
+        end
+      end
+
+      describe "#to_arguments" do
+        it "returns arguments representation of key" do
+          expect(key.to_arguments).to eq(arguments)
+        end
       end
     end
   end
