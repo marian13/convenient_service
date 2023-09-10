67b6e701982b396f285162699908d8c560a65924
Marian13 <mariankostyk13895@gmail.com>
2023-09-10 23:54:30 +0300

    refactor(has_j_send_result_short): update container

--- lib/convenient_service/service/plugins/has_j_send_result/container.rb
+++ lib/convenient_service/service/plugins/has_j_send_result/container.rb
@@ -11,6 +11,14 @@ module ConvenientService
             Commands::IsResult.call(result: result)
           end
 
+          export :"constants.DEFAULT_SUCCESS_CODE" do
+            Constants::DEFAULT_SUCCESS_CODE
+          end
+
+          export :"constants.DEFAULT_FAILURE_CODE" do
+            Constants::DEFAULT_FAILURE_CODE
+          end
+
           export :"constants.DEFAULT_ERROR_CODE" do
             Constants::DEFAULT_ERROR_CODE
           end
--- spec/lib/convenient_service/service/plugins/has_j_send_result/container_spec.rb
+++ spec/lib/convenient_service/service/plugins/has_j_send_result/container_spec.rb
@@ -13,12 +13,15 @@ RSpec.describe ConvenientService::Service::Plugins::HasJSendResult::Container do
     include ConvenientService::RSpec::Matchers::Export
 
     it { is_expected.to export(:"commands.is_result?") }
+    it { is_expected.to export(:"constants.DEFAULT_SUCCESS_CODE") }
+    it { is_expected.to export(:"constants.DEFAULT_FAILURE_CODE") }
+    it { is_expected.to export(:"constants.DEFAULT_ERROR_CODE") }
   end
 
   example_group "instance methods" do
     include ConvenientService::RSpec::Matchers::DelegateTo
 
-    describe "#is_result?" do
+    describe "#commands#is_result?" do
       let(:user_class) do
         Class.new do
           include ConvenientService::Support::DependencyContainer::Import
@@ -46,6 +49,54 @@ RSpec.describe ConvenientService::Service::Plugins::HasJSendResult::Container do
           .to delegate_to(ConvenientService::Service::Plugins::HasJSendResult::Commands::IsResult, :call)
           .with_arguments(result: result)
           .and_return_its_value
+      end
+    end
+
+    describe "#constants#DEFAULT_SUCCESS_CODE" do
+      let(:user_class) do
+        Class.new do
+          include ConvenientService::Support::DependencyContainer::Import
+
+          import :"constants.DEFAULT_SUCCESS_CODE", from: ConvenientService::Service::Plugins::HasJSendResult::Container
+        end
+      end
+
+      let(:user_instance) { user_class.new }
+
+      specify do
+        expect(user_instance.constants.DEFAULT_SUCCESS_CODE).to eq(ConvenientService::Service::Plugins::HasJSendResult::Constants::DEFAULT_SUCCESS_CODE)
+      end
+    end
+
+    describe "#constants#DEFAULT_FAILURE_CODE" do
+      let(:user_class) do
+        Class.new do
+          include ConvenientService::Support::DependencyContainer::Import
+
+          import :"constants.DEFAULT_FAILURE_CODE", from: ConvenientService::Service::Plugins::HasJSendResult::Container
+        end
+      end
+
+      let(:user_instance) { user_class.new }
+
+      specify do
+        expect(user_instance.constants.DEFAULT_FAILURE_CODE).to eq(ConvenientService::Service::Plugins::HasJSendResult::Constants::DEFAULT_FAILURE_CODE)
+      end
+    end
+
+    describe "#constants#DEFAULT_ERROR_CODE" do
+      let(:user_class) do
+        Class.new do
+          include ConvenientService::Support::DependencyContainer::Import
+
+          import :"constants.DEFAULT_ERROR_CODE", from: ConvenientService::Service::Plugins::HasJSendResult::Container
+        end
+      end
+
+      let(:user_instance) { user_class.new }
+
+      specify do
+        expect(user_instance.constants.DEFAULT_ERROR_CODE).to eq(ConvenientService::Service::Plugins::HasJSendResult::Constants::DEFAULT_ERROR_CODE)
       end
     end
   end
