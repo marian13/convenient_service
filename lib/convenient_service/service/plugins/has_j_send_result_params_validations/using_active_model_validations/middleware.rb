bc5bb7027fe94724a8d28a3770b68c731b37ff10
Marian13 <mariankostyk13895@gmail.com>
2023-09-11 03:52:04 +0300

    feat(using_active_model_validations): allow to pass status to middleware

--- lib/convenient_service/service/plugins/has_j_send_result_params_validations/using_active_model_validations/middleware.rb
+++ lib/convenient_service/service/plugins/has_j_send_result_params_validations/using_active_model_validations/middleware.rb
@@ -8,8 +8,11 @@ module ConvenientService
           class Middleware < MethodChainMiddleware
             intended_for :result, entity: :service
 
+            ##
+            # @return [ConvenientService::Service::Plugins::HasJSendResult::Entities::Result]
+            #
             def next(...)
-              return entity.failure(data: errors, message: errors.first.to_a.map(&:to_s).join(" ")) if errors.any?
+              return entity.public_send(status, data: errors, message: errors.first.to_a.map(&:to_s).join(" ")) if errors.any?
 
               chain.next(...)
             end
@@ -17,17 +20,25 @@ module ConvenientService
             private
 
             ##
-            # NOTE: errors method (from Active Model Validations) does NOT trigger validations
-            # that is why valid? is called here.
+            # @internal
+            #   NOTE: errors method (from Active Model Validations) does NOT trigger validations
+            #   that is why valid? is called here.
             #
-            # Check the following link for more info:
-            # https://guides.rubyonrails.org/active_record_validations.html#validations-overview-errors
+            #   Check the following link for more info:
+            #   - https://guides.rubyonrails.org/active_record_validations.html#validations-overview-errors
             #
-            # Quote (this method - errors):
-            # > This method is only useful after validations have been run, because it only inspects the errors collection and does not trigger validations itself.
+            #   Quote (this method - errors):
+            #   > This method is only useful after validations have been run, because it only inspects the errors collection and does not trigger validations itself.
             #
             def errors
               @errors ||= entity.tap(&:valid?).errors.messages.transform_values(&:first)
+            end
+
+            ##
+            # @return [Symbol]
+            #
+            def status
+              middleware_arguments.kwargs.fetch(:status) { :failure }
             end
           end
         end
--- spec/lib/convenient_service/service/plugins/has_j_send_result_params_validations/using_active_model_validations/middleware_spec.rb
+++ spec/lib/convenient_service/service/plugins/has_j_send_result_params_validations/using_active_model_validations/middleware_spec.rb
@@ -32,97 +32,183 @@ RSpec.describe ConvenientService::Service::Plugins::HasJSendResultParamsValidati
     end
   end
 
-  example_group "instance methods" do
-    describe "#call" do
-      include ConvenientService::RSpec::Helpers::WrapMethod
-      include ConvenientService::RSpec::Matchers::CallChainNext
-      include ConvenientService::RSpec::Matchers::Results
+  shared_examples "verify middleware behavior" do
+    example_group "instance methods" do
+      describe "#call" do
+        include ConvenientService::RSpec::Helpers::WrapMethod
+        include ConvenientService::RSpec::Matchers::CallChainNext
+        include ConvenientService::RSpec::Matchers::Results
 
-      subject(:method_value) { method.call }
+        subject(:method_value) { method.call }
 
-      let(:method) { wrap_method(service_instance, :result, observe_middleware: middleware) }
+        let(:method) { wrap_method(service_instance, :result, observe_middleware: middleware.with(status: status)) }
 
-      let(:service_class) do
-        Class.new.tap do |klass|
-          klass.class_exec(middleware) do |middleware|
-            include ConvenientService::Configs::Standard
+        let(:service_class) do
+          Class.new.tap do |klass|
+            klass.class_exec(status, middleware) do |status, middleware|
+              include ConvenientService::Configs::Standard
 
-            concerns do
-              use ConvenientService::Service::Plugins::HasJSendResultParamsValidations::UsingActiveModelValidations::Concern
-            end
+              concerns do
+                use ConvenientService::Service::Plugins::HasJSendResultParamsValidations::UsingActiveModelValidations::Concern
+              end
 
-            middlewares :result do
-              use_and_observe middleware
-            end
+              middlewares :result do
+                use_and_observe middleware.with(status: status)
+              end
 
-            attr_reader :foo
+              attr_reader :foo
 
-            ##
-            # NOTE: Rails 6 and 7 differently handle nested hashes in `validates` call. See links below.
-            # - https://github.com/rails/rails/blob/v6.0.0/activemodel/lib/active_model/translation.rb#L67
-            # - https://github.com/rails/rails/blob/v7.0.0/activemodel/lib/active_model/translation.rb#L67
-            # - https://github.com/rails/rails/commit/4645f2d34fc1d9f037096de988e5cc5ca41a3cf3
-            #
-            # For example, the following works in Ruby 2.7 + Rails 6, Ruby 2.7 + Rails 7, Ruby 3.* + Rails 7,
-            # but fails in Ruby 3.* and Rails 6.
-            #   validates :foo, length: {maximum: 2}
-            #
-            # To imitate a similar behaviour in Rails 6 a custom validation can be used.
-            #   validate { errors.add(:foo, "Foo length is over 2") if foo.to_s.length > 2 }
-            #   validate_with LengthValidatior, fields: :foo
-            #
-            # More info:
-            # - https://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations
-            #
-            # TODO: Add to troubleshoting.
-            #
-            # validates :foo, length: {maximum: 2}
-            validate { errors.add(:foo, "Foo length is over 2") if foo.to_s.length > 2 }
+              ##
+              # NOTE: Rails 6 and 7 differently handle nested hashes in `validates` call. See links below.
+              # - https://github.com/rails/rails/blob/v6.0.0/activemodel/lib/active_model/translation.rb#L67
+              # - https://github.com/rails/rails/blob/v7.0.0/activemodel/lib/active_model/translation.rb#L67
+              # - https://github.com/rails/rails/commit/4645f2d34fc1d9f037096de988e5cc5ca41a3cf3
+              #
+              # For example, the following works in Ruby 2.7 + Rails 6, Ruby 2.7 + Rails 7, Ruby 3.* + Rails 7,
+              # but fails in Ruby 3.* and Rails 6.
+              #   validates :foo, length: {maximum: 2}
+              #
+              # To imitate a similar behaviour in Rails 6 a custom validation can be used.
+              #   validate { errors.add(:foo, "Foo length is over 2") if foo.to_s.length > 2 }
+              #   validate_with LengthValidatior, fields: :foo
+              #
+              # More info:
+              # - https://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations
+              #
+              # TODO: Add to troubleshoting.
+              #
+              # validates :foo, length: {maximum: 2}
+              validate { errors.add(:foo, "Foo length is over 2") if foo.to_s.length > 2 }
 
-            def initialize(foo:)
-              @foo = foo
-            end
+              def initialize(foo:)
+                @foo = foo
+              end
 
-            ##
-            # IMPORTANT: Helps to avoid:
-            # *** ArgumentError Exception: Class name cannot be blank. You need to supply a name argument when anonymous class given
-            # https://stackoverflow.com/a/26168343/12201472
-            # https://api.rubyonrails.org/classes/ActiveModel/Naming.html
-            #
-            def self.name
-              "Service"
-            end
+              ##
+              # IMPORTANT: Helps to avoid:
+              # *** ArgumentError Exception: Class name cannot be blank. You need to supply a name argument when anonymous class given
+              # https://stackoverflow.com/a/26168343/12201472
+              # https://api.rubyonrails.org/classes/ActiveModel/Naming.html
+              #
+              def self.name
+                "Service"
+              end
 
-            def result
-              success
+              def result
+                success
+              end
             end
           end
         end
-      end
 
-      context "when validation does NOT have any errors" do
-        let(:service_instance) { service_class.new(foo: "x") }
+        context "when validation does NOT have any errors" do
+          let(:service_instance) { service_class.new(foo: "x") }
 
-        specify do
-          expect { method_value }
-            .to call_chain_next.on(method)
-            .and_return_its_value
-        end
-      end
-
-      context "when validation has any error" do
-        let(:service_instance) { service_class.new(foo: "bar") }
-
-        let(:errors) { service_instance.errors.messages.transform_values(&:first) }
-
-        it "returns failure with first error message for each invalid attribute as data" do
-          expect(method_value).to be_failure.with_data(errors)
+          specify do
+            expect { method_value }
+              .to call_chain_next.on(method)
+              .and_return_its_value
+          end
         end
 
-        it "returns failure with first error key/value joined by space as message" do
-          expect(method_value).to be_failure.with_message(errors.first.to_a.map(&:to_s).join(" "))
+        context "when validation has any error" do
+          let(:service_instance) { service_class.new(foo: "bar") }
+
+          let(:errors) { service_instance.errors.messages.transform_values(&:first) }
+
+          it "returns result with first error message for each invalid attribute as data" do
+            expect(method_value).to be_result(status).with_data(errors)
+          end
+
+          it "returns result with first error key/value joined by space as message" do
+            expect(method_value).to be_result(status).with_message(errors.first.to_a.map(&:to_s).join(" "))
+          end
         end
       end
+    end
+  end
+
+  context "when status is NOT passed" do
+    subject(:method_value) { method.call }
+
+    include ConvenientService::RSpec::Helpers::WrapMethod
+
+    let(:method) { wrap_method(service_instance, :result, observe_middleware: middleware) }
+
+    let(:service_class) do
+      Class.new.tap do |klass|
+        klass.class_exec(middleware) do |middleware|
+          include ConvenientService::Configs::Standard
+
+          concerns do
+            use ConvenientService::Service::Plugins::HasJSendResultParamsValidations::UsingActiveModelValidations::Concern
+          end
+
+          middlewares :result do
+            use_and_observe middleware
+          end
+
+          attr_reader :foo
+
+          ##
+          # NOTE: Rails 6 and 7 differently handle nested hashes in `validates` call. See links below.
+          # - https://github.com/rails/rails/blob/v6.0.0/activemodel/lib/active_model/translation.rb#L67
+          # - https://github.com/rails/rails/blob/v7.0.0/activemodel/lib/active_model/translation.rb#L67
+          # - https://github.com/rails/rails/commit/4645f2d34fc1d9f037096de988e5cc5ca41a3cf3
+          #
+          # For example, the following works in Ruby 2.7 + Rails 6, Ruby 2.7 + Rails 7, Ruby 3.* + Rails 7,
+          # but fails in Ruby 3.* and Rails 6.
+          #   validates :foo, length: {maximum: 2}
+          #
+          # To imitate a similar behaviour in Rails 6 a custom validation can be used.
+          #   validate { errors.add(:foo, "Foo length is over 2") if foo.to_s.length > 2 }
+          #   validate_with LengthValidatior, fields: :foo
+          #
+          # More info:
+          # - https://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations
+          #
+          # TODO: Add to troubleshoting.
+          #
+          # validates :foo, length: {maximum: 2}
+          validate { errors.add(:foo, "Foo length is over 2") if foo.to_s.length > 2 }
+
+          def initialize(foo:)
+            @foo = foo
+          end
+
+          ##
+          # IMPORTANT: Helps to avoid:
+          # *** ArgumentError Exception: Class name cannot be blank. You need to supply a name argument when anonymous class given
+          # https://stackoverflow.com/a/26168343/12201472
+          # https://api.rubyonrails.org/classes/ActiveModel/Naming.html
+          #
+          def self.name
+            "Service"
+          end
+
+          def result
+            success
+          end
+        end
+      end
+    end
+
+    let(:service_instance) { service_class.new(foo: "bar") }
+
+    it "defaults to `:failure`" do
+      expect(method_value).to be_failure
+    end
+  end
+
+  context "when status is failure" do
+    include_examples "verify middleware behavior" do
+      let(:status) { :failure }
+    end
+  end
+
+  context "when status is error" do
+    include_examples "verify middleware behavior" do
+      let(:status) { :error }
     end
   end
 end
