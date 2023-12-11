7d4bec8f460ed6e7bb65648c79765628e22a32e9
Marian13 <mariankostyk13895@gmail.com>
2023-12-11 23:58:15 +0200

    test(concern): add concern specs notes

---
+++ test/lib/convenient_service/dependencies/extractions/active_support_concern/concern_test.rb
@@ -0,0 +1,10 @@
+##
+# TODO: Direct specs.
+#
+# TODO: Rails Concern specs are using `assert_nothing_raised`. Should it be imported here?
+# - https://github.com/rails/rails/blob/v7.0.8/activesupport/test/concern_test.rb
+# - https://github.com/rails/rails/blob/v7.0.8/activesupport/test/fixtures/concern/some_concern.rb
+#
+# NOTE: Convenient Service Concern is slighlty different compared to Rails Concern.
+# See `lib/convenient_service/dependencies/extractions/active_support_concern.rb`.
+##
