a92a04002687c878618479804d0176e69b3d4fa4
Marian13 <mariankostyk13895@gmail.com>
2023-12-27 21:05:51 +0200

    feat(exception): allow to pass message or kwargs or no arguments

--- lib/convenient_service/exception.rb
+++ lib/convenient_service/exception.rb
@@ -1,16 +1,104 @@
 # frozen_string_literal: true
 
-##
-# Base class for all `ConvenientService` exceptions.
-# Can be used as a catch-all solution, for example:
-#
-#   begin
-#     any_service.result
-#   rescue ConvenientService::Exception => exception
-#     puts exception.message
-#   end
-#
 module ConvenientService
+  ##
+  # Base class for all `ConvenientService` exceptions.
+  #
+  # @example Can be used as a catch-all solution.
+  #
+  #   begin
+  #     any_service.result
+  #   rescue ConvenientService::Exception => exception
+  #     puts exception.message
+  #   end
+  #
   class Exception < ::StandardError
+    class << self
+      ##
+      # Creates `ConvenientService` exception instance.
+      # In contract to `StandardError.new`, may accept `kwargs`.
+      # In such a case the descendant must implement `initialize_with_kwargs` to specify the logic of how to generate a `message`.
+      # Without arguments behavior is also changed, it calls `initialize_without_arguments` instead of immediately setting `nil` as `message`.
+      #
+      # @note `initialize_with_kwargs` or `initialize_without_arguments` must call `initialize(message)`, otherwise exception instance won't be properly set up.
+      #
+      # @overload new
+      #   Calls `initialize_without_arguments` under the hood to generate the exception message.
+      #
+      #   @api private
+      #
+      #   @return [ConvenientService::Exception]
+      #
+      #   @example Usage.
+      #     module Exceptions
+      #       class Foo < ::ConvenientService::Exception
+      #         def initialize_without_arguments
+      #           # message = ...
+      #           initialize(message)
+      #         end
+      #       end
+      #     end
+      #
+      #     ::ConvenientService.raise Foo.new
+      #
+      # @overload new(message)
+      #   Calls `initialize(message)` under the hood to set the exception message.
+      #
+      #   @api private
+      #
+      #   @param message [String]
+      #
+      #   @return [ConvenientService::Exception]
+      #
+      #   @example Usage.
+      #     module Exceptions
+      #       class Foo < ::ConvenientService::Exception
+      #       end
+      #     end
+      #
+      #     ::ConvenientService.raise Foo.new("some message")
+      #
+      # @overload new(**kwargs)
+      #   Calls `initialize_with_kwargs(**kwargs)` under the hood to generate the exception message.
+      #
+      #   @api private
+      #
+      #   @param kwargs [Hash{Symbol => Object}]
+      #   @return [ConvenientService::Exception]
+      #
+      #   @example Usage.
+      #     module Exceptions
+      #       class Foo < ::ConvenientService::Exception
+      #         def initialize_with_kwargs(**kwargs)
+      #           # message = ...
+      #           initialize(message)
+      #         end
+      #       end
+      #     end
+      #
+      #     ::ConvenientService.raise Foo.new(foo: :bar, baz: :qux)
+      #
+      def new(message = nil, **kwargs)
+        if message
+          super(message)
+        elsif kwargs.any?
+          allocate.tap { |exception| exception.initialize_with_kwargs(**kwargs) }
+        else
+          allocate.tap(&:initialize_without_arguments)
+        end
+      end
+    end
+
+    ##
+    # @internal
+    #   NOTE: Two exceptions are equal when their classes, messages, and backtraces are equal.
+    #   - https://blog.arkency.com/2015/01/ruby-exceptions-equality
+    #
+    #   TODO: Compare skipping backtraces? Why?
+    #
+    # def ==(other)
+    #   # ...
+    # end
+    ##
   end
 end
--- spec/lib/convenient_service/exception_spec.rb
+++ spec/lib/convenient_service/exception_spec.rb
@@ -4,10 +4,60 @@ require "spec_helper"
 
 require "convenient_service"
 
+# rubocop:disable RSpec/NestedGroups
 RSpec.describe ConvenientService::Exception do
-  include ConvenientService::RSpec::PrimitiveMatchers::BeDescendantOf
+  example_group "inheritance" do
+    include ConvenientService::RSpec::PrimitiveMatchers::BeDescendantOf
 
-  it "is `StandardError` descendant" do
-    expect(described_class).to be_descendant_of(StandardError)
+    subject { described_class }
+
+    it { is_expected.to be_descendant_of(StandardError) }
+  end
+
+  example_group "class methods" do
+    describe ".new" do
+      context "when NO arguments are passed" do
+        let(:exception_class) do
+          Class.new(described_class) do
+            def initialize_without_arguments
+              initialize("initialize_without_arguments message")
+            end
+          end
+        end
+
+        let(:message) { "initialize_without_arguments message" }
+
+        it "delegates to `#initialize_without_arguments`" do
+          expect(exception_class.new.message).to eq(message)
+        end
+      end
+
+      context "when `message` is passed" do
+        let(:exception_class) { Class.new(described_class) }
+        let(:message) { "foo" }
+
+        it "delegates to `StandartError#initialize`" do
+          expect(exception_class.new(message).message).to eq(message)
+        end
+      end
+
+      context "when `kwargs` are passed" do
+        let(:exception_class) do
+          Class.new(described_class) do
+            def initialize_with_kwargs(**kwargs)
+              initialize("initialize_with_kwargs #{kwargs}")
+            end
+          end
+        end
+
+        let(:kwargs) { {foo: :bar} }
+        let(:message) { "initialize_with_kwargs #{kwargs}" }
+
+        it "delegates to `#initialize_with_kwargs`" do
+          expect(exception_class.new(**kwargs).message).to eq(message)
+        end
+      end
+    end
   end
 end
+# rubocop:enable RSpec/NestedGroups
