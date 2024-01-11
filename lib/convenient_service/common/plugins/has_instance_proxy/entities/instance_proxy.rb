488c94e9d33ccca2c13797ebd624233e6ee0007e
Marian13 <mariankostyk13895@gmail.com>
2024-01-11 02:44:37 +0200

    test(has_instance_proxy): add specs

--- lib/convenient_service/common/plugins/has_instance_proxy/entities/instance_proxy.rb
+++ lib/convenient_service/common/plugins/has_instance_proxy/entities/instance_proxy.rb
@@ -10,18 +10,12 @@ module ConvenientService
             # @param target [Object] Can be any type.
             # @return [void]
             #
-            # @internal
-            #   TODO: Direct Specs.
-            #
             def initialize(target:)
               @__convenient_service_instance_proxy_target__ = target
             end
 
             ##
             # @return [Object] Can be any type.
-            #
-            # @internal
-            #   TODO: Direct Specs.
             #
             def instance_proxy_target
               @__convenient_service_instance_proxy_target__
--- spec/lib/convenient_service/common/plugins/has_instance_proxy/entities/instance_proxy_spec.rb
+++ spec/lib/convenient_service/common/plugins/has_instance_proxy/entities/instance_proxy_spec.rb
@@ -1,3 +1,24 @@
-##
-# TODO: Specs.
-##
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "convenient_service"
+
+# rubocop:disable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
+RSpec.describe ConvenientService::Common::Plugins::HasInstanceProxy::Entities::InstanceProxy do
+  let(:instance_proxy) { described_class.new(target: target) }
+  let(:target) { "foo" }
+
+  example_group "instance methods" do
+    describe "#instance_proxy_target" do
+      it "returns `target` passed to constructor" do
+        expect(instance_proxy.instance_proxy_target).to eq(target)
+      end
+
+      it "uses `@__convenient_service_instance_proxy_target__` as instance variable" do
+        expect(instance_proxy.instance_proxy_target).to eq(instance_proxy.instance_variable_get(:@__convenient_service_instance_proxy_target__))
+      end
+    end
+  end
+end
+# rubocop:enable RSpec/NestedGroups, RSpec/MultipleMemoizedHelpers
