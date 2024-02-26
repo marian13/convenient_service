d6e2b7da0a0114872db8483c8c2489164556628c
Marian13 <mariankostyk13895@gmail.com>
2024-02-27 01:26:50 +0200

    refactor(callbacks): remove outdated container

--- lib/convenient_service/common/plugins/has_callbacks.rb
+++ lib/convenient_service/common/plugins/has_callbacks.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 require_relative "has_callbacks/concern"
-require_relative "has_callbacks/container"
 require_relative "has_callbacks/entities"
 require_relative "has_callbacks/middleware"
