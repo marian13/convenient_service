6c452022ecd163e0ab769af3da1b8345938b1369
Marian13 <mariankostyk13895@gmail.com>
2024-03-02 22:42:01 +0200

    test(can_have_steps): add specs for allow to override inputs in result method steps

--- spec/lib/convenient_service/service/plugins/can_have_steps/entities/step/plugins/can_be_method_step/can_be_executed/middleware_spec.rb
+++ spec/lib/convenient_service/service/plugins/can_have_steps/entities/step/plugins/can_be_method_step/can_be_executed/middleware_spec.rb
@@ -481,69 +481,7 @@ RSpec.describe ConvenientService::Service::Plugins::CanHaveSteps::Entities::Step
             expect { method_value }.not_to call_chain_next.on(method)
           end
 
-          context "when `organizer` does NOT have own `:result` method" do
-            let(:container) do
-              Class.new.tap do |klass|
-                klass.class_exec(middleware) do |middleware|
-                  include ConvenientService::Service::Configs::Minimal
-
-                  self::Step.class_exec(middleware) do |middleware|
-                    middlewares :result do
-                      observe middleware
-                    end
-                  end
-
-                  step :result
-                end
-              end
-            end
-
-            let(:exception_message) do
-              <<~TEXT
-                Service `#{container}` tries to use `:result` method in a step, but it is NOT defined.
-
-                Did you forget to define it?
-              TEXT
-            end
-
-            it "raises `ConvenientService::Service::Plugins::CanHaveSteps::Entities::Step::Plugins::CanBeMethodStep::CanBeExecuted::Exceptions::MethodForStepIsNotDefined`" do
-              expect { method_value }
-                .to raise_error(ConvenientService::Service::Plugins::CanHaveSteps::Entities::Step::Plugins::CanBeMethodStep::CanBeExecuted::Exceptions::MethodForStepIsNotDefined)
-                .with_message(exception_message)
-            end
-
-            specify do
-              expect { ignoring_exception(ConvenientService::Service::Plugins::CanHaveSteps::Entities::Step::Plugins::CanBeMethodStep::CanBeExecuted::Exceptions::MethodForStepIsNotDefined) { method_value } }
-                .to delegate_to(ConvenientService, :raise)
-            end
-          end
-
           context "when `organizer` has own `:result` method" do
-            let(:container) do
-              Class.new.tap do |klass|
-                klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
-                  include ConvenientService::Service::Configs::Minimal
-
-                  self::Step.class_exec(middleware) do |middleware|
-                    middlewares :result do
-                      observe middleware
-                    end
-                  end
-
-                  step :result
-
-                  def result
-                    success
-                  end
-
-                  ##
-                  # NOTE: Used to confirm that own is called, not prepended method.
-                  #
-                  prepend prepend_module
-                end
-              end
-            end
-
             let(:prepend_module) do
               Module.new.tap do |mod|
                 def result
@@ -552,18 +490,298 @@ RSpec.describe ConvenientService::Service::Plugins::CanHaveSteps::Entities::Step
               end
             end
 
-            it "calls that own method" do
-              ##
-              # NOTE: Own method returns `success`, while prepended returns `failure`.
-              # See `organizer_service_class` definition above.
-              #
-              expect(method_value).to be_success
+            context "when that own `:result` method does NOT accept any kwargs" do
+              let(:container) do
+                Class.new.tap do |klass|
+                  klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                    include ConvenientService::Service::Configs::Minimal
+
+                    self::Step.class_exec(middleware) do |middleware|
+                      middlewares :result do
+                        observe middleware
+                      end
+                    end
+
+                    step :result
+
+                    def result
+                      success
+                    end
+
+                    ##
+                    # NOTE: Used to confirm that own is called, not prepended method.
+                    #
+                    prepend prepend_module
+                  end
+                end
+              end
+
+              it "calls that own method without inputs" do
+                ##
+                # NOTE: Own method returns `success`, while prepended returns `failure`.
+                # See `organizer_service_class` definition above.
+                #
+                expect(method_value).to be_success
+              end
+
+              specify do
+                expect { method_value }
+                  .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                  .with_arguments(step.organizer.class, step.method, private: true)
+              end
             end
 
-            specify do
-              expect { method_value }
-                .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
-                .with_arguments(step.organizer.class, step.method, private: true)
+            context "when that own `:result` method accepts kwargs" do
+              context "when that own `:result` method accepts some required kwargs" do
+                context "when inputs does NOT have that required kwarg" do
+                  let(:container) do
+                    Class.new.tap do |klass|
+                      klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                        include ConvenientService::Service::Configs::Minimal
+
+                        self::Step.class_exec(middleware) do |middleware|
+                          middlewares :result do
+                            observe middleware
+                          end
+                        end
+
+                        step :result
+
+                        def result(required_kwarg:)
+                          success(data: {required_kwarg: required_kwarg})
+                        end
+
+                        ##
+                        # NOTE: Used to confirm that own is called, not prepended method.
+                        #
+                        prepend prepend_module
+                      end
+                    end
+                  end
+
+                  it "raises `ArgumentError`" do
+                    expect { method_value }.to raise_error(ArgumentError)
+                  end
+
+                  specify do
+                    expect { ignoring_exception(ArgumentError) { method_value } }
+                      .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                      .with_arguments(step.organizer.class, step.method, private: true)
+                  end
+                end
+
+                context "when inputs have that required kwarg" do
+                  let(:container) do
+                    Class.new.tap do |klass|
+                      klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                        include ConvenientService::Service::Configs::Minimal
+
+                        self::Step.class_exec(middleware) do |middleware|
+                          middlewares :result do
+                            observe middleware
+                          end
+                        end
+
+                        step :result,
+                          in: {required_kwarg: -> { :required_kwarg_value }}
+
+                        def result(required_kwarg:)
+                          success(data: {required_kwarg: required_kwarg})
+                        end
+
+                        ##
+                        # NOTE: Used to confirm that own is called, not prepended method.
+                        #
+                        prepend prepend_module
+                      end
+                    end
+                  end
+
+                  it "calls that own method with that required kwarg" do
+                    ##
+                    # NOTE: Own method returns `success`, while prepended returns `failure`.
+                    # See `organizer_service_class` definition above.
+                    #
+                    expect(method_value).to be_success.with_data(required_kwarg: :required_kwarg_value)
+                  end
+
+                  specify do
+                    expect { method_value }
+                      .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                      .with_arguments(step.organizer.class, step.method, private: true)
+                  end
+                end
+              end
+
+              context "when that own `:result` method accepts some optional kwargs" do
+                context "when inputs does NOT have that optional kwarg" do
+                  let(:container) do
+                    Class.new.tap do |klass|
+                      klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                        include ConvenientService::Service::Configs::Minimal
+
+                        self::Step.class_exec(middleware) do |middleware|
+                          middlewares :result do
+                            observe middleware
+                          end
+                        end
+
+                        step :result
+
+                        def result(optional_kwarg: :optional_kwarg_default_value)
+                          success(data: {optional_kwarg: optional_kwarg})
+                        end
+
+                        ##
+                        # NOTE: Used to confirm that own is called, not prepended method.
+                        #
+                        prepend prepend_module
+                      end
+                    end
+                  end
+
+                  it "calls that own method ignoring that optional kwarg" do
+                    ##
+                    # NOTE: Own method returns `success`, while prepended returns `failure`.
+                    # See `organizer_service_class` definition above.
+                    #
+                    expect(method_value).to be_success.with_data(optional_kwarg: :optional_kwarg_default_value)
+                  end
+
+                  specify do
+                    expect { method_value }
+                      .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                      .with_arguments(step.organizer.class, step.method, private: true)
+                  end
+                end
+
+                context "when inputs have that optional kwarg" do
+                  let(:container) do
+                    Class.new.tap do |klass|
+                      klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                        include ConvenientService::Service::Configs::Minimal
+
+                        self::Step.class_exec(middleware) do |middleware|
+                          middlewares :result do
+                            observe middleware
+                          end
+                        end
+
+                        step :result,
+                          in: {optional_kwarg: -> { :optional_kwarg_value }}
+
+                        def result(optional_kwarg: :optional_kwarg_default_value)
+                          success(data: {optional_kwarg: optional_kwarg})
+                        end
+
+                        ##
+                        # NOTE: Used to confirm that own is called, not prepended method.
+                        #
+                        prepend prepend_module
+                      end
+                    end
+                  end
+
+                  it "calls that own method with that optional kwarg" do
+                    ##
+                    # NOTE: Own method returns `success`, while prepended returns `failure`.
+                    # See `organizer_service_class` definition above.
+                    #
+                    expect(method_value).to be_success.with_data(optional_kwarg: :optional_kwarg_value)
+                  end
+
+                  specify do
+                    expect { method_value }
+                      .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                      .with_arguments(step.organizer.class, step.method, private: true)
+                  end
+                end
+              end
+
+              context "when that own `:result` method accepts rest kwargs" do
+                context "when inputs does NOT have any kwarg" do
+                  let(:container) do
+                    Class.new.tap do |klass|
+                      klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                        include ConvenientService::Service::Configs::Minimal
+
+                        self::Step.class_exec(middleware) do |middleware|
+                          middlewares :result do
+                            observe middleware
+                          end
+                        end
+
+                        step :result
+
+                        def result(**kwargs)
+                          success(data: {kwargs: kwargs})
+                        end
+
+                        ##
+                        # NOTE: Used to confirm that own is called, not prepended method.
+                        #
+                        prepend prepend_module
+                      end
+                    end
+                  end
+
+                  it "calls that own method with empty kwargs" do
+                    ##
+                    # NOTE: Own method returns `success`, while prepended returns `failure`.
+                    # See `organizer_service_class` definition above.
+                    #
+                    expect(method_value).to be_success.with_data(kwargs: {})
+                  end
+
+                  specify do
+                    expect { method_value }
+                      .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                      .with_arguments(step.organizer.class, step.method, private: true)
+                  end
+                end
+
+                context "when inputs have any extra kwarg" do
+                  let(:container) do
+                    Class.new.tap do |klass|
+                      klass.class_exec(prepend_module, middleware) do |prepend_module, middleware|
+                        include ConvenientService::Service::Configs::Minimal
+
+                        self::Step.class_exec(middleware) do |middleware|
+                          middlewares :result do
+                            observe middleware
+                          end
+                        end
+
+                        step :result,
+                          in: {extra_kwarg: -> { :extra_kwarg_value }}
+
+                        def result(**kwargs)
+                          success(data: {kwargs: kwargs})
+                        end
+
+                        ##
+                        # NOTE: Used to confirm that own is called, not prepended method.
+                        #
+                        prepend prepend_module
+                      end
+                    end
+                  end
+
+                  it "calls that own method with that optional kwarg" do
+                    ##
+                    # NOTE: Own method returns `success`, while prepended returns `failure`.
+                    # See `organizer_service_class` definition above.
+                    #
+                    expect(method_value).to be_success.with_data(kwargs: {extra_kwarg: :extra_kwarg_value})
+                  end
+
+                  specify do
+                    expect { method_value }
+                      .to delegate_to(ConvenientService::Utils::Module, :get_own_instance_method)
+                      .with_arguments(step.organizer.class, step.method, private: true)
+                  end
+                end
+              end
             end
           end
         end
