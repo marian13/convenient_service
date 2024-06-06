f15724011de2e7a312ff994e876bdf1e2be4a677
Marian13 <mariankostyk13895@gmail.com>
2024-06-06 23:51:12 +0300

    chore(has_mermaid_flowchart): add note that plugin is experimental

--- lib/convenient_service/service/plugins/has_mermaid_flowchart/concern.rb
+++ lib/convenient_service/service/plugins/has_mermaid_flowchart/concern.rb
@@ -3,13 +3,18 @@
 module ConvenientService
   module Service
     module Plugins
+      ##
+      # @api private
+      #
+      # @note `HasMermaidFlowchart` is experimental. It is NOT decided yet whether its support will be continued. For example, it does NOT work with `or_step`.
+      #
       module HasMermaidFlowchart
         module Concern
           include Support::Concern
 
           class_methods do
             ##
-            # @api public
+            # @api private
             #
             # @return [ConvenientService::Service::Plugins::HasMermaidFlowchart::Entities::Flowchart]
             #
