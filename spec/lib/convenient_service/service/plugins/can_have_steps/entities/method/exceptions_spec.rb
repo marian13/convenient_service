034976514de00d79d52ce284164d9e2464c211f1
Marian13 <mariankostyk13895@gmail.com>
2024-07-20 23:56:59 +0300

    refactor(steps): remove validate since it is not used since v0.11

--- spec/lib/convenient_service/service/plugins/can_have_steps/entities/method/exceptions_spec.rb
+++ spec/lib/convenient_service/service/plugins/can_have_steps/entities/method/exceptions_spec.rb
@@ -8,15 +8,7 @@ RSpec.describe ConvenientService::Service::Plugins::CanHaveSteps::Entities::Meth
   include ConvenientService::RSpec::PrimitiveMatchers::BeDescendantOf
 
   specify { expect(described_class::MethodHasNoOrganizer).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::InputMethodIsNotDefinedInContainer).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::OutputMethodIsDefinedInContainer).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::AliasInputMethodIsNotDefinedInContainer).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::AliasOutputMethodIsDefinedInContainer).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::OutputMethodProc).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::OutputMethodRawValue).to be_descendant_of(ConvenientService::Exception) }
   specify { expect(described_class::CallerCanNotCalculateReassignment).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::InputMethodReassignment).to be_descendant_of(ConvenientService::Exception) }
-  specify { expect(described_class::MethodIsNotInputMethod).to be_descendant_of(ConvenientService::Exception) }
   specify { expect(described_class::MethodIsNotOutputMethod).to be_descendant_of(ConvenientService::Exception) }
   specify { expect(described_class::OutMethodStepIsNotCompleted).to be_descendant_of(ConvenientService::Exception) }
 end
