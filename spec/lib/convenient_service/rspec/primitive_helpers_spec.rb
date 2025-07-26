5375726ff5934bfb9120886eeac4617cab64d337
Marian13 <mariankostyk13895@gmail.com>
2025-07-26 19:07:58 +0300

    test(rspec): add more specs for PrimitiveHelpers

---
+++ spec/lib/convenient_service/rspec/primitive_helpers_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+##
+# @author Marian Kostyk <mariankostyk13895@gmail.com>
+# @license LGPLv3 <https://www.gnu.org/licenses/lgpl-3.0.html>
+##
+
+require "spec_helper"
+
+require "convenient_service"
+
+RSpec.describe ConvenientService::RSpec::PrimitiveHelpers, type: :standard do
+  example_group "modules" do
+    include ConvenientService::RSpec::Matchers::IncludeModule
+
+    subject { described_class }
+
+    it { is_expected.to include_module(ConvenientService::Support::Concern) }
+
+    context "when included" do
+      subject { klass }
+
+      let(:klass) do
+        Class.new.tap do |klass|
+          klass.class_exec(described_class) do |mod|
+            include mod
+          end
+        end
+      end
+
+      it { is_expected.to include_module(described_class::IgnoringException) }
+      it { is_expected.to include_module(described_class::InThreads) }
+    end
+  end
+end
