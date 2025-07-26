e835011f90164224afceb675c263cb279911b833
Marian13 <mariankostyk13895@gmail.com>
2025-07-26 04:02:26 +0300

    test(extractions): add BacktraceCleaner#clean_frame tests

--- test/lib/convenient_service/dependencies/extractions/active_support_backtrace_cleaner/backtrace_cleaner_test.rb
+++ test/lib/convenient_service/dependencies/extractions/active_support_backtrace_cleaner/backtrace_cleaner_test.rb
@@ -159,3 +159,140 @@ class BacktraceCleanerDefaultFilterAndSilencerTest < Minitest::Test
     assert_equal backtrace, @bc.clean(backtrace)
   end
 end
+
+##
+# NOTE: Same as above but for `clean_frame`.
+#
+class BacktraceCleanerFrameFilterTest < Minitest::Test
+  def setup
+    @bc = ConvenientService::Dependencies::Extractions::ActiveSupportBacktraceCleaner::BacktraceCleaner.new
+    @bc.add_filter { |line| line.gsub("/my/prefix", "") }
+  end
+
+  should "backtrace should filter all lines in a backtrace, removing prefixes" do
+    assert_equal \
+      ["/my/class.rb", "/my/module.rb"],
+      ["/my/prefix/my/class.rb", "/my/prefix/my/module.rb"].map { |frame| @bc.clean_frame(frame) }
+  end
+
+  should "backtrace cleaner should allow removing filters" do
+    @bc.remove_filters!
+    assert_equal "/my/prefix/my/class.rb", @bc.clean_frame("/my/prefix/my/class.rb")
+  end
+
+  should "backtrace should contain unaltered lines if they don't match a filter" do
+    assert_equal "/my/other_prefix/my/class.rb", @bc.clean_frame("/my/other_prefix/my/class.rb")
+  end
+
+  should "#dup also copy filters" do
+    copy = @bc.dup
+    @bc.add_filter { |line| line.gsub("/other/prefix/", "") }
+
+    assert_equal "my/class.rb", @bc.clean_frame("/other/prefix/my/class.rb")
+    assert_equal "/other/prefix/my/class.rb", copy.clean_frame("/other/prefix/my/class.rb")
+  end
+end
+
+class BacktraceCleanerFrameSilencerTest < Minitest::Test
+  def setup
+    @bc = ConvenientService::Dependencies::Extractions::ActiveSupportBacktraceCleaner::BacktraceCleaner.new
+    @bc.add_silencer { |line| line.include?("mongrel") }
+  end
+
+  should "backtrace should not contain lines that match the silencer" do
+    assert_equal \
+      [nil, "/other/class.rb", nil],
+      ["/mongrel/class.rb", "/other/class.rb", "/mongrel/stuff.rb"].map { |frame| @bc.clean_frame(frame) }
+  end
+
+  should "backtrace cleaner should allow removing silencer" do
+    @bc.remove_silencers!
+    assert_equal "/mongrel/stuff.rb", @bc.clean_frame("/mongrel/stuff.rb")
+  end
+
+  should "#dup also copy silencers" do
+    copy = @bc.dup
+
+    @bc.add_silencer { |line| line.include?("puma") }
+    assert_equal nil, @bc.clean_frame("/puma/stuff.rb")
+    assert_equal "/puma/stuff.rb", copy.clean_frame("/puma/stuff.rb")
+  end
+end
+
+class BacktraceCleanerMultipleFrameSilencersTest < Minitest::Test
+  def setup
+    @bc = ConvenientService::Dependencies::Extractions::ActiveSupportBacktraceCleaner::BacktraceCleaner.new
+    @bc.add_silencer { |line| line.include?("mongrel") }
+    @bc.add_silencer { |line| line.include?("yolo") }
+  end
+
+  should "backtrace should not contain lines that match the silencers" do
+    assert_equal \
+      [nil, "/other/class.rb", nil, nil],
+      ["/mongrel/class.rb", "/other/class.rb", "/mongrel/stuff.rb", "/other/yolo.rb"].map { |frame| @bc.clean_frame(frame) }
+  end
+
+  should "backtrace should only contain lines that match the silencers" do
+    assert_equal \
+      ["/mongrel/class.rb", nil, "/mongrel/stuff.rb", "/other/yolo.rb"],
+      ["/mongrel/class.rb", "/other/class.rb", "/mongrel/stuff.rb", "/other/yolo.rb"].map { |frame| @bc.clean_frame(frame, :noise) }
+  end
+end
+
+class BacktraceCleanerFrameFilterAndSilencerTest < Minitest::Test
+  def setup
+    @bc = ConvenientService::Dependencies::Extractions::ActiveSupportBacktraceCleaner::BacktraceCleaner.new
+    @bc.add_filter { |line| line.gsub("/mongrel", "") }
+    @bc.add_silencer { |line| line.include?("mongrel") }
+  end
+
+  should "backtrace should not silence lines that has first had their silence hook filtered out" do
+    assert_equal "/class.rb", @bc.clean_frame("/mongrel/class.rb")
+  end
+end
+
+class BacktraceCleanerFrameDefaultFilterAndSilencerTest < Minitest::Test
+  def setup
+    @bc = ConvenientService::Dependencies::Extractions::ActiveSupportBacktraceCleaner::BacktraceCleaner.new
+  end
+
+  should "should format installed gems correctly" do
+    backtrace = ["#{Gem.default_dir}/gems/nosuchgem-1.2.3/lib/foo.rb"]
+    result = @bc.clean_frame(backtrace.first, :all)
+    assert_equal "nosuchgem (1.2.3) lib/foo.rb", result
+  end
+
+  should "should format installed gems not in Gem.default_dir correctly" do
+    target_dir = Gem.path.detect { |p| p != Gem.default_dir }
+    # skip this test if default_dir is the only directory on Gem.path
+    if target_dir
+      backtrace = ["#{target_dir}/gems/nosuchgem-1.2.3/lib/foo.rb"]
+      result = @bc.clean_frame(backtrace.first, :all)
+      assert_equal "nosuchgem (1.2.3) lib/foo.rb", result
+    end
+  end
+
+  should "should format gems installed by bundler" do
+    backtrace = ["#{Gem.default_dir}/bundler/gems/nosuchgem-1.2.3/lib/foo.rb"]
+    result = @bc.clean_frame(backtrace.first, :all)
+    assert_equal "nosuchgem (1.2.3) lib/foo.rb", result
+  end
+
+  should "should silence gems from the backtrace" do
+    backtrace = ["#{Gem.path[0]}/gems/nosuchgem-1.2.3/lib/foo.rb"]
+    result = @bc.clean_frame(backtrace.first)
+    assert_nil result
+  end
+
+  should "should silence stdlib" do
+    backtrace = ["#{RbConfig::CONFIG["rubylibdir"]}/lib/foo.rb"]
+    result = @bc.clean_frame(backtrace.first)
+    assert_nil result
+  end
+
+  should "should preserve lines that have a subpath matching a gem path" do
+    backtrace = [Gem.default_dir, *Gem.path].map { |path| "/parent#{path}/gems/nosuchgem-1.2.3/lib/foo.rb" }
+
+    assert_equal backtrace, backtrace.map { |frame| @bc.clean_frame(frame) }
+  end
+end
