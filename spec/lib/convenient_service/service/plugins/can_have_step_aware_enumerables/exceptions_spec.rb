fa73f04c45ba140a93adb1a37bba9745f4c7d2c8
Marian13 <mariankostyk13895@gmail.com>
2025-07-29 22:23:07 +0300

    test(can_have_step_aware_enumerables): add specs for exceptions

---
+++ spec/lib/convenient_service/service/plugins/can_have_step_aware_enumerables/exceptions_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+##
+# @author Marian Kostyk <mariankostyk13895@gmail.com>
+# @license LGPLv3 <https://www.gnu.org/licenses/lgpl-3.0.html>
+##
+
+require "spec_helper"
+
+require "convenient_service"
+
+RSpec.describe ConvenientService::Service::Plugins::CanHaveStepAwareEnumerables::Exceptions, type: :standard do
+  include ConvenientService::RSpec::PrimitiveMatchers::BeDescendantOf
+
+  specify { expect(described_class::ObjectIsNotEnumerable).to be_descendant_of(ConvenientService::Exception) }
+  specify { expect(described_class::ObjectIsNotEnumerator).to be_descendant_of(ConvenientService::Exception) }
+  specify { expect(described_class::AlreadyUsedTerminalChaining).to be_descendant_of(ConvenientService::Exception) }
+  specify { expect(described_class::InvalidEvaluateByValue).to be_descendant_of(ConvenientService::Exception) }
+end
