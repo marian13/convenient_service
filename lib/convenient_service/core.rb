b725e77f8669d274b3c9f1beaa4e1cdb022f07ee
Marian13 <mariankostyk13895@gmail.com>
2025-12-05 17:28:38 +0200

    feat(core): introduce ConvenientService::Core.entity?

--- lib/convenient_service/core.rb
+++ lib/convenient_service/core.rb
@@ -28,5 +28,73 @@ module ConvenientService
       #
       __convenient_service_config__
     end
+
+    class << self
+      ##
+      # Checks whether an object is a Convenient Service entity class.
+      #
+      # @api public
+      #
+      # @param entity_class [Object] Can be any type.
+      # @return [Boolean]
+      #
+      # @example Simple usage.
+      #   class Service
+      #     include ConvenientService::Standard::Config
+      #
+      #     def result
+      #       success
+      #     end
+      #   end
+      #
+      #   service = Service.new
+      #
+      #   ConvenientService::Core.entity_class?(Service)
+      #   # => true
+      #
+      #   ConvenientService::Core.entity_class?(service)
+      #   # => false
+      #
+      #   ConvenientService::Core.entity?(42)
+      #   # => false
+      #
+      def entity_class?(entity_class)
+        return false unless entity_class.instance_of?(::Class)
+
+        entity_class.include?(::ConvenientService::Core)
+      end
+
+      ##
+      # Checks whether an object is a Convenient Service entity instance.
+      #
+      # @api public
+      #
+      # @param entity [Object] Can be any type.
+      # @return [Boolean]
+      #
+      # @example Simple usage.
+      #   class Service
+      #     include ConvenientService::Standard::Config
+      #
+      #     def result
+      #       success
+      #     end
+      #   end
+      #
+      #   service = Service.new
+      #
+      #   ConvenientService::Core.entity?(service)
+      #   # => true
+      #
+      #   ConvenientService::Core.entity?(Service)
+      #   # => false
+      #
+      #   ConvenientService::Core.entity?(42)
+      #   # => false
+      #
+      def entity?(entity)
+        entity_class?(entity.class)
+      end
+    end
   end
 end
--- spec/lib/convenient_service/core_spec.rb
+++ spec/lib/convenient_service/core_spec.rb
@@ -9,10 +9,12 @@ require "spec_helper"
 
 require "convenient_service"
 
+# rubocop:disable RSpec/NestedGroups
 RSpec.describe ConvenientService::Core, type: :standard do
+  include ConvenientService::RSpec::Matchers::DelegateTo
+
   example_group "modules" do
     include ConvenientService::RSpec::Matchers::IncludeModule
-    include ConvenientService::RSpec::Matchers::DelegateTo
 
     subject { described_class }
 
@@ -35,4 +37,146 @@ RSpec.describe ConvenientService::Core, type: :standard do
       end
     end
   end
+
+  example_group "class methods" do
+    describe ".entity_class?" do
+      context "when `entity` is NOT class" do
+        let(:entity_class) { 42 }
+
+        it "returns `false`" do
+          expect(described_class.entity_class?(entity_class)).to eq(false)
+        end
+      end
+
+      context "when `entity` is class" do
+        context "when `entity` is NOT entity class" do
+          let(:entity_class) { Class.new }
+
+          it "returns `false`" do
+            expect(described_class.entity_class?(entity_class)).to eq(false)
+          end
+        end
+
+        context "when `entity` is entity class" do
+          let(:service_class) do
+            Class.new do
+              include ConvenientService::Standard::Config
+
+              def result
+                success
+              end
+            end
+          end
+
+          let(:entity_class) { service_class }
+
+          it "returns `true`" do
+            expect(described_class.entity_class?(entity_class)).to eq(true)
+          end
+        end
+      end
+
+      example_group "comprehensive suite" do
+        let(:first_step) do
+          Class.new do
+            include ConvenientService::Standard::Config
+
+            def result
+              success
+            end
+          end
+        end
+
+        let(:service_class) do
+          Class.new.tap do |klass|
+            klass.class_exec(first_step) do |first_step|
+              include ConvenientService::Standard::Config
+
+              step first_step
+            end
+          end
+        end
+
+        let(:service_instance) { service_class.new }
+
+        specify { expect(described_class.entity_class?(service_class)).to eq(true) }
+        specify { expect(described_class.entity_class?(service_class.result_class)).to eq(true) }
+        specify { expect(described_class.entity_class?(service_class.result_class.data_class)).to eq(true) }
+        specify { expect(described_class.entity_class?(service_class.result_class.message_class)).to eq(true) }
+        specify { expect(described_class.entity_class?(service_class.result_class.code_class)).to eq(true) }
+        specify { expect(described_class.entity_class?(service_class.result_class.status_class)).to eq(true) }
+        specify { expect(described_class.entity_class?(service_class.step_class)).to eq(true) }
+
+        specify { expect(described_class.entity_class?(service_instance)).to eq(false) }
+        specify { expect(described_class.entity_class?(service_instance.result)).to eq(false) }
+        specify { expect(described_class.entity_class?(service_instance.result.unsafe_data)).to eq(false) }
+        specify { expect(described_class.entity_class?(service_instance.result.unsafe_message)).to eq(false) }
+        specify { expect(described_class.entity_class?(service_instance.result.unsafe_code)).to eq(false) }
+        specify { expect(described_class.entity_class?(service_instance.result.status)).to eq(false) }
+        specify { expect(described_class.entity_class?(service_instance.steps.first)).to eq(false) }
+      end
+    end
+
+    describe ".entity?" do
+      let(:service_class) do
+        Class.new do
+          include ConvenientService::Standard::Config
+
+          def result
+            success
+          end
+        end
+      end
+
+      let(:service_instance) { service_class.new }
+
+      specify do
+        expect { described_class.entity?(service_instance) }
+          .to delegate_to(described_class, :entity_class?)
+          .with_arguments(service_class)
+          .and_return_its_value
+      end
+
+      example_group "comprehensive suite" do
+        let(:first_step) do
+          Class.new do
+            include ConvenientService::Standard::Config
+
+            def result
+              success
+            end
+          end
+        end
+
+        let(:service_class) do
+          Class.new.tap do |klass|
+            klass.class_exec(first_step) do |first_step|
+              include ConvenientService::Standard::Config
+
+              step first_step
+            end
+          end
+        end
+
+        let(:service_instance) { service_class.new }
+
+        specify { expect(described_class.entity?(service_class)).to eq(false) }
+        specify { expect(described_class.entity?(service_class.result_class)).to eq(false) }
+        specify { expect(described_class.entity?(service_class.result_class.data_class)).to eq(false) }
+        specify { expect(described_class.entity?(service_class.result_class.message_class)).to eq(false) }
+        specify { expect(described_class.entity?(service_class.result_class.code_class)).to eq(false) }
+        specify { expect(described_class.entity?(service_class.result_class.status_class)).to eq(false) }
+        specify { expect(described_class.entity?(service_class.step_class)).to eq(false) }
+
+        specify { expect(described_class.entity?(service_instance)).to eq(true) }
+        specify { expect(described_class.entity?(service_instance.result)).to eq(true) }
+        specify { expect(described_class.entity?(service_instance.result.unsafe_data)).to eq(true) }
+        specify { expect(described_class.entity?(service_instance.result.unsafe_message)).to eq(true) }
+        specify { expect(described_class.entity?(service_instance.result.unsafe_code)).to eq(true) }
+        specify { expect(described_class.entity?(service_instance.result.status)).to eq(true) }
+        specify { expect(described_class.entity?(service_instance.steps.first)).to eq(true) }
+      end
+    end
+  end
 end
+# rubocop:enable RSpec/NestedGroups
