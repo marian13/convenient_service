e4c7dc13158280b833a00bee4787e8ef82a7c0ad
Marian13 <mariankostyk13895@gmail.com>
2025-12-20 02:41:51 +0200

    test(strings): add more tests

--- spec/e2e/string_representation_spec.rb
+++ spec/e2e/string_representation_spec.rb
@@ -74,44 +74,82 @@ RSpec.describe "String representation", type: [:standard, :e2e] do
     expect(service_instance.inspect).to eq("<Service>")
   end
 
-  specify do
-    expect(result_class.to_s).to match(/#<Class:.+?>::Result/) # "#<Class:0x00007f5ad2929120>::Result"
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(result_class.to_s).to match(/#<Class:.+?>::Result/) # "#<Class:0x00007f5ad2929120>::Result"
+    end
+
+    specify do
+      expect(result_class.inspect).to match(/#<Class:.+?>::Result/) # "#<Class:0x00007f5ad2929120>::Result"
+    end
+  else
+    specify do
+      expect(result_class.to_s).to match(/#<Class:.+?>/) # "#<Class:0x00007f5ad2929120>"
+    end
+
+    specify do
+      expect(result_class.inspect).to match(/#<Class:.+?>/) # "#<Class:0x00007f5ad2929120>"
+    end
   end
 
-  specify do
-    expect(result_class.inspect).to match(/#<Class:.+?>::Result/) # "#<Class:0x00007f5ad2929120>::Result"
-  end
-
-  specify do
-    expect(success.to_s).to match(/#<#<Class:.+?>::Result:.+?>/) # "#<#<Class:0x00007f01b3802da0>::Result:0x00007f01b28f9778>"
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(success.to_s).to match(/#<#<Class:.+?>::Result:.+?>/) # "#<#<Class:0x00007f01b3802da0>::Result:0x00007f01b28f9778>"
+    end
+  else
+    specify do
+      expect(success.to_s).to match(/#<#<Class:.+?>:.+?>/) # "#<#<Class:0x00005589aae9cdf0>:0x00005589aaf1c348>"
+    end
   end
 
   specify do
     expect(success.inspect).to eq("<Service::Result status: :success, data_keys: [:foo, :baz]>")
   end
 
-  specify do
-    expect(failure.to_s).to match(/#<#<Class:.+?>::Result:.+?>/) # "#<#<Class:0x00007f01b3802da0>::Result:0x00007f01b28f9778>"
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(failure.to_s).to match(/#<#<Class:.+?>::Result:.+?>/) # "#<#<Class:0x00007f01b3802da0>::Result:0x00007f01b28f9778>"
+    end
+  else
+    specify do
+      expect(failure.to_s).to match(/#<#<Class:.+?>:.+?>/) # "#<#<Class:0x00005589aae9cdf0>:0x00005589aaf1c348>"
+    end
   end
 
   specify do
     expect(failure.inspect).to eq("<Service::Result status: :failure, message: \"foo\">")
   end
 
-  specify do
-    expect(error.to_s).to match(/#<#<Class:.+?>::Result:.+?>/) # "#<#<Class:0x00007f01b3802da0>::Result:0x00007f01b28f9778>"
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(error.to_s).to match(/#<#<Class:.+?>::Result:.+?>/) # "#<#<Class:0x00007f01b3802da0>::Result:0x00007f01b28f9778>"
+    end
+  else
+    specify do
+      expect(error.to_s).to match(/#<#<Class:.+?>:.+?>/) # "#<#<Class:0x00005589aae9cdf0>:0x00005589aaf1c348>"
+    end
   end
 
   specify do
     expect(error.inspect).to eq("<Service::Result status: :error, message: \"foo\">")
   end
 
-  specify do
-    expect(data_class.to_s).to match(/#<Class:.+?>::Result::Data/) # #<Class:0x00007f0155b1ac80>::Result::Data
-  end
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(data_class.to_s).to match(/#<Class:.+?>::Result::Data/) # #<Class:0x00007f0155b1ac80>::Result::Data
+    end
 
-  specify do
-    expect(data_class.inspect).to match(/#<Class:.+?>::Result::Data/) # #<Class:0x00007f0155b1ac80>::Result::Data
+    specify do
+      expect(data_class.inspect).to match(/#<Class:.+?>::Result::Data/) # #<Class:0x00007f0155b1ac80>::Result::Data
+    end
+  else
+    specify do
+      expect(data_class.to_s).to match(/#<Class:.+?>/) # #<Class:0x00007f0155b1ac80>
+    end
+
+    specify do
+      expect(data_class.inspect).to match(/#<Class:.+?>/) # #<Class:0x00007f0155b1ac80>
+    end
   end
 
   specify do
@@ -125,12 +163,22 @@ RSpec.describe "String representation", type: [:standard, :e2e] do
     expect(data_instance.inspect).to eq("<Service::Result::Data foo: :bar, baz: :qux>")
   end
 
-  specify do
-    expect(message_class.to_s).to match(/#<Class:.+?>::Result::Message/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Message"
-  end
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(message_class.to_s).to match(/#<Class:.+?>::Result::Message/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Message"
+    end
 
-  specify do
-    expect(message_class.inspect).to match(/#<Class:.+?>::Result::Message/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Message"
+    specify do
+      expect(message_class.inspect).to match(/#<Class:.+?>::Result::Message/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Message"
+    end
+  else
+    specify do
+      expect(message_class.to_s).to match(/#<Class:.+?>/) # "#<Class:0x00007f8b2d9fa3c0>"
+    end
+
+    specify do
+      expect(message_class.inspect).to match(/#<Class:.+?>/) # "#<Class:0x00007f8b2d9fa3c0>"
+    end
   end
 
   specify do
@@ -141,12 +189,22 @@ RSpec.describe "String representation", type: [:standard, :e2e] do
     expect(message_instance.inspect).to eq("<Service::Result::Message text: \"foo\">")
   end
 
-  specify do
-    expect(code_class.to_s).to match(/#<Class:.+?>::Result::Code/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Code"
-  end
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(code_class.to_s).to match(/#<Class:.+?>::Result::Code/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Code"
+    end
 
-  specify do
-    expect(code_class.inspect).to match(/#<Class:.+?>::Result::Code/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Code"
+    specify do
+      expect(code_class.inspect).to match(/#<Class:.+?>::Result::Code/) # "#<Class:0x00007f8b2d9fa3c0>::Result::Code"
+    end
+  else
+    specify do
+      expect(code_class.to_s).to match(/#<Class:.+?>/) # "#<Class:0x00007f8b2d9fa3c0>"
+    end
+
+    specify do
+      expect(code_class.inspect).to match(/#<Class:.+?>/) # "#<Class:0x00007f8b2d9fa3c0>"
+    end
   end
 
   specify do
@@ -157,12 +215,22 @@ RSpec.describe "String representation", type: [:standard, :e2e] do
     expect(code_instance.inspect).to eq("<Service::Result::Code value: :default_error>")
   end
 
-  specify do
-    expect(step_class.to_s).to match(/#<Class:.+?>::Step/) # "#<Class:0x00007f848eabad28>::Step"
-  end
+  if ConvenientService::Dependencies.ruby.version >= 3.0
+    specify do
+      expect(step_class.to_s).to match(/#<Class:.+?>::Step/) # "#<Class:0x00007f848eabad28>::Step"
+    end
 
-  specify do
-    expect(step_class.inspect).to match(/#<Class:.+?>::Step/) # "#<Class:0x00007f848eabad28>::Step"
+    specify do
+      expect(step_class.inspect).to match(/#<Class:.+?>::Step/) # "#<Class:0x00007f848eabad28>::Step"
+    end
+  else
+    specify do
+      expect(step_class.to_s).to match(/#<Class:.+?>/) # "#<Class:0x00007f848eabad28>"
+    end
+
+    specify do
+      expect(step_class.inspect).to match(/#<Class:.+?>/) # "#<Class:0x00007f848eabad28>"
+    end
   end
 
   specify do
